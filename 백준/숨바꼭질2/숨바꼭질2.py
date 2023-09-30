8dd25c2cfcf0957d8dbf2c2f9943784a1dbb8e16
MKSonny <minson9@naver.com>
2023-09-30 22:06:31 +0900

    백준) 숨바꼭질2 문제 시도

---
+++ 백준/숨바꼭질2/숨바꼭질2.py
@@ -0,0 +1,34 @@
+# x - 1 or x + 1 or 2x
+# 5 17
+cnt = 0
+min_time = 99999
+str = input()
+n, m = map(int, str.split())
+time = []
+
+def dfs(l, n, m):
+    global min_time
+    global cnt
+
+    if l == 0:
+        dfs(l + 1, n + 1, m)
+        dfs(l + 1, n - 1, m)
+        dfs(l + 1, 2 * n, m)
+
+    else:
+        if n < m:
+            dfs(l + 1, n + 1, m)
+            dfs(l + 1, 2 * n, m)
+        elif n == m:
+            time.append(l)
+            if min_time > l:
+                min_time = l
+            return
+        else:
+            dfs(l + 1, n - 1, m)
+
+dfs(0, 5, 17)
+for item in time:
+    if item == min_time:
+        cnt += 1
+print(min_time, cnt)
