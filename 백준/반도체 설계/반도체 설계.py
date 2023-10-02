7eeb56759af88d2545787cc4522dd2d164d2f18c
MKSonny <minson9@naver.com>
2023-10-02 16:50:32 +0900

    백준) 반도체 설계 문제 정답은 맞았지만 더 정확한 방법 분석

--- 백준/반도체 설계/반도체 설계.py
+++ 백준/반도체 설계/반도체 설계.py
@@ -1,13 +1,33 @@
-min_ = 99999
-a = [4, 2, 6, 3, 1, 5]
-def binary_solve(a, left, right):
-    global min_
-    mid = (left + right)// 2
-    if min_ > a[mid]:
-        print('a')
-        min_ = a[mid]
+n = input()
+a = list(map(int, input().split()))
+def binary_search(A, key, low, high):
+    if low > high:
+        return low
+    mid = (low + high) // 2
+    if key == A[mid]:
+        return mid
+    elif key < A[mid]:
+        return binary_search(A, key, low, mid - 1)
     else:
-        return binary_solve(a, left, mid)
+        return binary_search(A, key, mid + 1, high)
 
-anw = binary_solve(a, 0, len(a) - 1)
-print(min_)
+res = [a[0]]
+
+for i in range(1, len(a)):
+    if res[-1] < a[i]:
+        res.append(a[i])
+    else:
+        # print(res)
+        res[binary_search(res, a[i], 0, len(res) - 1)] = a[i]
+print(len(res))
+'''
+else:
+    # 0, len(res) - 1: 이렇게 하면 절대로
+    # binary_search(0, len(res) - 1, arr[i])를 통해 나오는 mid 값은
+    # res의 max index 보다 커질 수 없다.
+    idx = binary_search(0, len(res) - 1, arr[i])
+
+    print('idx', binary_search(0, len(res) - 1, arr[i]))
+    print('res', res)
+    res[binary_search(0, len(res) - 1, arr[i])] = arr[i]
+'''
