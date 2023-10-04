3d38981254dfa7bb5722f51f7a3875ba8ea7d266
MKSonny <minson9@naver.com>
2023-10-04 14:13:36 +0900

    백준) 유기농 배추 성공

--- 백준/유기농 배추.py
+++ 백준/유기농 배추.py
@@ -1,38 +1,31 @@
+import sys
+sys.setrecursionlimit(10000)
+cnt = 0
+def dfs(x, y):
+    if 0 <= x < r and 0 <= y < c:
+        if field[y][x] == 1:
+            # left
+            field[y][x] = 0
+            dfs(x, y - 1)
+            # right
+            dfs(x, y + 1)
+            # up
+            dfs(x - 1, y)
+            # down
+            dfs(x + 1, y)
+
 t = int(input())
 
-cnt = 0
-
-def dfs(a, row, col, visited):
-    global cnt
-    global max_row, max_col
-    if row > max_row or row < 0 or col > max_col or col < 0:
-        return
-    if a[row][col] == 1 and visited[row][col] == 0:
-        cnt += 1
-        visited[row][col] = 1
-        # left
-        dfs(a, row, col - 1, visited)
-        # right
-        dfs(a, row, col + 1, visited)
-        # up
-        dfs(a, row - 1, col, visited)
-        # down
-        dfs(a, row + 1, col, visited)
-
-for k in range(t):
-    data = list(map(int, input().split()))
-    field = [[0 for _ in range(data[0])] for _ in range(data[1])]
-    visited = [[0 for _ in range(data[0])] for _ in range(data[1])]
-    max_row = data[1]
-    max_col = data[0]
-    for i in range(data[2]):
-        location = list(map(int, input().split()))
-        x = location[1]
-        y = location[0]
-        field[x][y] = 1
-    for i in range(data[1]):
-        for j in range(data[0]):
-            dfs(field, i, j, visited)
-
-for row in field:
-    print(row)
+for _ in range(t):
+    r, c, m = map(int, input().split())
+    field = [[0 for _ in range(r)] for _ in range(c)]
+    cnt = 0
+    for i in range(m):
+        x, y = map(int, input().split())
+        field[y][x] = 1
+    for i in range(r):
+        for j in range(c):
+            if field[j][i] == 1:
+                dfs(i, j)
+                cnt += 1
+    print(cnt)
