895188324a6f2a71b1454c2cbf2eb0fe6ad7447c
MKSonny <minson9@naver.com>
2023-10-09 20:22:16 +0900

    알고리즘) 삽입 정렬 하향식으로 -> 교과서 정답으로 수정

--- 알고리즘 수업/4장 연습문제/삽입 정렬 하향식으로.py
+++ 알고리즘 수업/4장 연습문제/삽입 정렬 하향식으로.py
@@ -10,20 +10,15 @@ def insertion_sort(a):
 
 
 def insertion_sort_recursive(a, n):
-    if n <= 1:
-        return
-
+    if n == 1: return
     insertion_sort_recursive(a, n - 1)
 
     key = a[n - 1]
     j = n - 2
-
-    while j >= 0 and key < a[j]:
+    while j >= 0 and a[j] > key:
         a[j + 1] = a[j]
         j -= 1
-
     a[j + 1] = key
-
 
 data = [5, 3, 8, 4, 9, 1, 2, 7]
 print(data)
