02347b2f60141492db3c1bd50e2e11b2a9de99ff
MKSonny <minson9@naver.com>
2023-10-11 18:27:14 +0900

    알고리즘) 병합 정렬에서 merge 함수 내부에 if a[i] < a[j]: 라고 적으면 안정성을 만족하지 않을 수도 있다.

--- 알고리즘 수업/3, 4, 5장/전체.py
+++ 알고리즘 수업/3, 4, 5장/전체.py
@@ -64,8 +64,35 @@ def binary_search(a, left, right, key):
             return binary_search(a, left, mid - 1, key)
     return -1
 
+def merge(a, left, mid, right):
+    i = left
+    j = mid + 1
+    k = left
+    sorted = [0] * len(a)
+    while i <= mid and j <= right:
+        # 어떻게 해야 안정성을 만족하는가?
+        if a[i] <= a[j]:
+            sorted[k] = a[i]
+            i, k = i + 1, k + 1
+        else:
+            sorted[k] = a[j]
+            j, k = j + 1, k + 1
+
+    if i > mid:
+        sorted[k: k + right - j + 1] = a[j: right + 1]
+    else:
+        sorted[k: k + mid - i + 1] = a[i: mid + 1]
+
+    a[left: right + 1] = sorted[left: right + 1]
+
+
+def merge_sort(a, left, right):
+    if left < right:
+        mid = (left + right) // 2
+        merge_sort(a, left, mid)
+        merge_sort(a, mid + 1, right)
+        merge(a, left, mid, right)
+
 data = [5, 2, 1, 9, 10]
-quick_sort(data, 0, len(data) - 1)
+merge_sort(data, 0, len(data) - 1)
 print(data)
-idx = quick_select(data, 0, len(data) - 1, 1)
-print(idx)
