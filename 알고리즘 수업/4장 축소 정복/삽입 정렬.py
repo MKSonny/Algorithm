9667f198a183ec47a3d67abf079f4ed4754c4734
MKSonny <minson9@naver.com>
2023-10-12 11:54:44 +0900

    알고리즘) 삽입 정렬: while j >= 0 and A[j] >= key로 해야 안정 정렬?

--- 알고리즘 수업/4장 축소 정복/삽입 정렬.py
+++ 알고리즘 수업/4장 축소 정복/삽입 정렬.py
@@ -3,11 +3,11 @@ def insertion_sort(A):
     for i in range(1, n):
         key = A[i]
         j = i - 1
-        while j >= 0 and A[j] > key:
+        while j >= 0 and A[j] >= key:
             A[j + 1] = A[j]
             j -= 1
         A[j + 1] = key
-data = [5, 3, 8, 4, 9, 1, 2, 7]
+data = [9, 8, 7, 5, 6, 5]
 print(data)
 insertion_sort(data)
 print(data)
