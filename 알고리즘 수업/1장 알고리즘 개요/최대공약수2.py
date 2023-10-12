337d09f43420a0c105ba45ba870c094339c6fbcf
MKSonny <minson9@naver.com>
2023-10-12 11:08:05 +0900

    알고리즘) 최대 공약수

--- 알고리즘 수업/1장 알고리즘 개요/최대공약수2.py
+++ 알고리즘 수업/1장 알고리즘 개요/최대공약수2.py
@@ -1,8 +1,15 @@
 def gcd(a, b):
-    while b != 0:
-        r = a % b
-        a = b
-        b = r
-    return a
+    list_a = []
+    list_b = []
+    for i in range(1, a + 1):
+        if a % i == 0:
+            list_a.append(i)
 
-print(gcd(28, 12))
+    for i in range(1, b + 1):
+        if b % i == 0:
+            list_b.append(i)
+
+    print(list_a)
+    print(list_b)
+
+gcd(12, 18)
