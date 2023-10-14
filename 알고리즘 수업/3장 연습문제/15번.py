11d03898bdfe4dd3467cec8f729c09a86912adce
MKSonny <minson9@naver.com>
2023-10-14 11:40:18 +0900

    알고리즘) 3장 해밀턴 경로 찾기

---
+++ 알고리즘 수업/3장 연습문제/15번.py
@@ -0,0 +1,48 @@
+def is_safe(g, v, pos, path):
+    if g[path[pos - 1]][v] == 0:
+        return False
+    for vertex in path:
+        if vertex == v:
+            return False
+    return True
+
+
+def hamiltonian_recur(g, path, pos):
+    n = len(g)
+    if pos == n:
+        if g[path[pos - 1]][path[0]] == 1:
+            return True
+        else:
+            return False
+
+    for v in range(1, n):
+        if is_safe(g, v, pos, path) == True:
+            path[pos] = v
+            if hamiltonian_recur(g, path, pos + 1) == True:
+                return True
+            path[pos] = -1
+    return False
+
+
+def hamiltonian_cycle(g):
+    n = len(g)
+    path = [-1] * (n + 1)
+    path[0] = path[n] = 0
+
+    if hamiltonian_recur(g, path, 1) == False:
+        print("해밀토니언 사이클 없음")
+        return False
+    else:
+        print("해밀토니안 사이클: ", path)
+    return True
+
+
+g1 = [
+    [0, 1, 0, 1, 0],
+    [1, 0, 1, 1, 1],
+    [0, 1, 0, 0, 1],
+    [1, 1, 0, 0, 1],
+    [0, 1, 1, 1, 0]
+]
+
+hamiltonian_cycle(g1)
---
+++ 알고리즘 수업/3장 연습문제/18번.py
@@ -0,0 +1,7 @@
+temp = []
+cnt = 0
+sum = 0
+data = [(10, 60), (20, 100), (30, 120)]
+
+all_combinations = [[0, 1] for _ in range(len(data))]
+print(all_combinations)
