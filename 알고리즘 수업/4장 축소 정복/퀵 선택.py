df618c263e778c975c33d921c3d41d819ca287fa
MKSonny <minson9@naver.com>
2023-10-15 18:18:22 +0900

    알고리즘) quick_select 구현 추가

---
+++ 알고리즘 수업/4장 축소 정복/퀵 선택.py
@@ -0,0 +1,26 @@
+def partition(a, left, right):
+    pivot = a[left]
+    low = left + 1
+    high = right
+    while low <= high:
+        while low <= right and a[low] <= pivot: low += 1
+        while high >= left and a[high] > pivot: high -= 1
+        if low < high:
+            a[low], a[high] = a[high], a[low]
+
+    a[left], a[high] = a[high], a[left]
+
+    return high
+
+def quick_select(a, left, right, k):
+    pos = partition(a, left, right)
+    if pos + 1 == left + k:
+        return a[pos]
+    elif pos + 1 < left + k:
+        return quick_select(a, pos + 1, right, left - (pos + 1) + k)
+    else:
+        return quick_select(a, left, pos - 1, k)
+
+
+data = [1, 2, 3, 4, 5]
+print(quick_select(data, 0, len(data) - 1, 3))
