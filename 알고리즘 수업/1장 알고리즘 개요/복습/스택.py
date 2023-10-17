c1ffd030e3d0fd214ed535a5fdd433f11e576ee5
MKSonny <minson9@naver.com>
2023-10-17 18:33:19 +0900

    알고리즘) 스택

---
+++ 알고리즘 수업/1장 알고리즘 개요/복습/스택.py
@@ -0,0 +1,33 @@
+index = int(input())
+
+stack = []
+
+for i in range(index):
+    string = list(input().split())
+    order = string[0]
+    # push, pop, size, top, empty
+    if order == 'push':
+        value = string[1]
+        stack.append(value)
+
+    elif order == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+
+    elif order == 'size':
+        print(len(stack))
+
+    elif order == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+
+    elif order == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+
