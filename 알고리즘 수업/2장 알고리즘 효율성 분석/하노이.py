a8ed593820db8cc44b26926dc3760663e3664f15
MKSonny <minson9@naver.com>
2023-10-18 19:48:00 +0900

    알고리즘) 유기농 배추

--- 알고리즘 수업/2장 알고리즘 효율성 분석/하노이.py
+++ 알고리즘 수업/2장 알고리즘 효율성 분석/하노이.py
@@ -1,11 +1,56 @@
-def hanoi_tower(n, fr, tmp, to):
-    if n == 1:
-        print('원판 1: %s ---> %s' % (fr, to))
-        return 1
-    else:
-        cnt1 = hanoi_tower(n - 1, fr, to, tmp)
-        print('원판 %d: %s ---> %s' % (n, fr, to))
-        cnt2 = hanoi_tower(n - 1, tmp, fr, to)
-        return 1 + cnt1 + cnt2
+def partition(a, left, right):
+    low = left + 1
+    pivot = a[left]
+    high = right
 
-print(hanoi_tower(4, 'A', 'B', 'C'))
+    while low <= high:
+        while low <= right and a[low] <= pivot:
+            low += 1
+        while high >= left and a[high] > pivot:
+            high -= 1
+        if low < high:
+            a[low], a[high] = a[high], a[low]
+
+    a[high], a[left] = a[left], a[high]
+    return high
+
+def merge(a, left, mid, right):
+    i = left
+    k = left
+    j = mid + 1
+    tmp = [0] * len(a)
+
+    while i <= mid and j <= right:
+        if a[i] <= a[j]:
+            tmp[k] = a[i]
+            i += 1
+        else:
+            tmp[k] = a[j]
+            j += 1
+        k += 1
+
+    if i > mid:
+        tmp[k : k + right - j + 1] = a[j : right + 1]
+    else:
+        tmp[k : k + mid - i + 1] = a[i : mid + 1]
+    a[left : right + 1] = tmp[left : right + 1]
+
+
+def merge_sort(a, left, right):
+    if left < right:
+        mid = (left + right) // 2
+        merge_sort(a, left, mid)
+        merge_sort(a, mid + 1, right)
+        merge(a, left, mid, right)
+
+def quick_sort(a, left, right):
+    if left < right:
+        pos = partition(a, left, right)
+        quick_sort(a, left, pos - 1)
+        quick_sort(a, pos + 1, right)
+
+
+data = [4, 2, 1, 9, 10, 3]
+merge_sort(data, 0, len(data) - 1)
+# quick_sort(data, 0, len(data) - 1)
+print(data)
