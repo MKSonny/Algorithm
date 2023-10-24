3c6425f43abe6df0b4bfeebb9cce2fec9bb394e6
MKSonny <minson9@naver.com>
2023-10-24 20:25:01 +0900

    알고리즘) 동적 계획법 이항 정리 적용

--- 알고리즘 수업/7장 동적 계획법/이항정리.py
+++ 알고리즘 수업/7장 동적 계획법/이항정리.py
@@ -3,3 +3,18 @@ def bino_coef_dc(n, k):
     if k == 0 or k == n:
         return 1
     return bino_coef_dc(n - 1, k - 1) + bino_coef_dc(n - 1, k)
+
+def bino_coef_dp(n, k):
+    C = [[- 1 for _ in range(k + 1)] for _ in range(n + 1)]
+
+    for i in range(n + 1):
+        for j in range(min(i, k) + 1):
+            if j == 0 or j == i:
+                C[i][j] = 1
+            else:
+                C[i][j] = C[i - 1][j - 1] + C[i - 1][j]
+
+    return C[n][k]
+
+print(bino_coef_dc(4, 2))
+print(bino_coef_dp(4, 2))
