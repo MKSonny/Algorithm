c81e6175c2435198d0071741b6277de223197350
MKSonny <minson9@naver.com>
2023-10-25 17:41:40 +0900

    알고리즘) 유기농 배추 dfs 추가

--- 알고리즘 수업/5장 분할 정복 기법/유기농 배추.py
+++ 알고리즘 수업/5장 분할 정복 기법/유기농 배추.py
@@ -4,6 +4,22 @@ T = int(input()) #테스트케이스의 개수
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
+
+def dfs(x, y):
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    # 상,하,좌,우 확인
+    for i in range(4):
+        nx = x + dx[i]
+    ny = y + dy[i]
+
+    if (0 <= nx < N) and (0 <= ny < M):
+        if matrix[nx][ny] == 1:
+            matrix[nx][ny] = -1
+        dfs(nx, ny)
+
+
 def BFS(x,y):
     queue = [(x,y)]
     matrix[x][y] = 0 # 방문처리
