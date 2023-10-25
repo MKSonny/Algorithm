fc2f362fc36229d91a9c00ac7ca724734549aae5
MKSonny <minson9@naver.com>
2023-10-25 20:50:49 +0900

    알고리즘) 유기농 내가 푼 방식 성공

--- 알고리즘 수업/5장 분할 정복 기법/유기농 배추.py
+++ 알고리즘 수업/5장 분할 정복 기법/유기농 배추.py
@@ -1,24 +1,36 @@
 test_case = int(input())
-
-# left, right, down, up
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
+# 가로길이(열) M(1 ≤ M ≤ 50)
+# 세로길이(행) N(1 ≤ N ≤ 50)
 # 행렬만들기
-for i in range(T):
+
+def dfs(y, x):
+    if (0 <= y < N) and (0 <= x < M) and matrix[y][x] == 1:
+        matrix[y][x] = -1
+        # 위
+        dfs(y - 1, x)
+        # 아래
+        dfs(y + 1, x)
+        # 왼쪽
+        dfs(y, x - 1)
+        # 오른쪽
+        dfs(y, x + 1)
+
+for i in range(test_case):
     # M, N 사용 주의
     M, N, K = map(int, input().split())
     matrix = [[0]*(M) for _ in range(N)]
     cnt = 0
 
     for j in range(K):
-        x,y = map(int, input().split())
-        matrix[x][y] = 1
+        x, y = map(int, input().split())
+        matrix[y][x] = 1
 
-    for a in range(N):
-        for b in range(M):
-            if matrix[a][b] == 1:
-                dfs(a,b)
+    # 가로길이(열) M(1 ≤ M ≤ 50)
+    # 세로길이(행) N(1 ≤ N ≤ 50)
+    for y in range(N):
+        for x in range(M):
+            if matrix[y][x] == 1:
+                dfs(y, x)
                 cnt += 1
 
     print(cnt)
