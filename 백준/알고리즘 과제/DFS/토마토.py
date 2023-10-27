46ce8af7308bd10cf831d480f5ec4f21ce75221a
MKSonny <minson9@naver.com>
2023-10-27 20:24:08 +0900

    알고리즘) 토마토 시간 초과로 틀리게 된다.

--- 백준/알고리즘 과제/DFS/토마토.py
+++ 백준/알고리즘 과제/DFS/토마토.py
@@ -6,19 +6,18 @@ dy = [0,0,-1,1]
 m, n = map(int, input().split())
 # 토마토 받아서 넣기. 이차원 리스트로 만들어질거.
 matrix = [list(map(int, input().split())) for _ in range(n)]
-print(matrix)
-
-
-def print_queue_contents(q):
-    elements_copy = list(q.queue)
-    for element in elements_copy:
-        print(element)
-
-
 
 cnt = 0
+zero_cnt = 0
 def bfs(q):
+    global cnt
+    global zero_cnt
+
+    if q.qsize() == 0:
+        cnt -= 1
+        return
     q2 = queue.Queue()
+    cnt += 1
 
     while not q.empty():
         (y, x) = q.get()
@@ -28,19 +27,30 @@ def bfs(q):
 
             if (0 <= nx < m) and (0 <= ny < n) and matrix[ny][nx] == 0:
                 matrix[ny][nx] = 1
+                zero_cnt += 1
                 q2.put((ny, nx))
-    print(q2.qsize())
+    # print(q2.qsize())
     bfs(q2)
 
 q = queue.Queue()
+
+in_zero = 0
 
 for i in range(n):
     for j in range(m):
         if matrix[i][j] == 1:
             q.put((i, j))
-            bfs(q)
+        elif matrix[i][j] == 0:
+            in_zero += 1
+bfs(q)
 
-print(matrix)
+# print(zero_cnt, in_zero)
+
+if zero_cnt != in_zero:
+    cnt = -1
+# print(zero_cnt, in_zero)
+
+# print(matrix)
 
 print(cnt)
 '''
