e71d845feffda9cbd02638e6f3f6a22e0bd052b3
MKSonny <minson9@naver.com>
2023-10-28 16:30:01 +0900

    알고리즘) 연구소 주석 제거 -> 치킨 배달 문제는 조합을 이전 재귀로 구현한 방식으로 사용하면 된다

--- 백준/알고리즘 과제/브루트 포스/연구소.py
+++ 백준/알고리즘 과제/브루트 포스/연구소.py
@@ -8,8 +8,9 @@ for _ in range(n):
     row = list(map(int, input().split()))  # 각 줄에서 숫자들을 공백으로 구분하여 리스트로 변환
     matrix.append(row)
 
-def combination(a, n):
+def combination(a):
     all_combi = []
+    n = len(a)
     for i in range(n - 2):
         for j in range(i + 1, n - 1):
             for k in range(j + 1, n):
@@ -33,20 +34,12 @@ for i in range(n):
         elif matrix[i][j] == 2:
             virus.append((i, j))
 
-# print(zeros)
-
 all_combi = combination(zeros, len(zeros))
-# print(matrix)
 
 prev = [[[0] for _ in range(m)] for _ in range(n)]
 prev[:] = matrix[:]
 
-# print('prev', prev)
-#
-# print(virus)
-
 def bfs(q):
-    # q.put((x, y))
     cnt = 0
 
     while not q.empty():
@@ -67,40 +60,22 @@ def bfs(q):
 
     return cnt
 
-# print()
 cnt = 0
 max = 0
 for value in all_combi:
     matrix = [row[:] for row in prev]
-    # print(prev)
-    # print(value)
     wall_a, wall_b, wall_c = value[0], value[1], value[2]
-
-    # print(wall_a, wall_b, wall_c)
 
     matrix[wall_a[0]][wall_a[1]] = 1
     matrix[wall_b[0]][wall_b[1]] = 1
     matrix[wall_c[0]][wall_c[1]] = 1
 
-    # for row in matrix:
-    #     print(row)
-
     for spread in virus:
         q.put((spread[0], spread[1]))
-    aa = bfs(q)
-    if max < aa:
-        max = aa
+
+    safe_area = bfs(q)
+
+    if max < safe_area:
+        max = safe_area
 
 print(max)
-
-
-'''
-7 7
-2 0 0 0 1 1 0
-0 0 1 0 1 2 0
-0 1 1 0 1 0 0
-0 1 0 0 0 0 0
-0 0 0 0 0 1 1
-0 1 0 0 0 0 0
-0 1 0 0 0 0 0
-'''
