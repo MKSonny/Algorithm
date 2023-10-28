02c1b2910c5706a0b003f9f95f5e01a50525f2ed
MKSonny <minson9@naver.com>
2023-10-28 17:59:47 +0900

    알고리즘) 정렬: 보석 도둑

---
+++ 백준/알고리즘 과제/Sorting/보석 도둑.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+
+dia = []
+bag = []
+
+for _ in range(n):
+    dia.append(list(map(int, input().split())))
+
+for _ in range(m):
+    bag.append(int(input()))
+
+
+
+for j in range(n):
+    for i in range(n):
+        dia[i][j]
+
+
+print(dia)
+print(bag)
---
+++ 백준/알고리즘 과제/분할 정복/별 찍기 10.py
@@ -0,0 +1,14 @@
+def devide(n):
+    for i in range(n):
+        print('*', end='')
+    print()
+    for i in range(n//3):
+        print('* *', end='')
+    print()
+    for i in range(n):
+        print('*', end='')
+    print()
+
+
+devide(27)
+
