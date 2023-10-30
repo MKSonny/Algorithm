a8578df76ff7130b8a5301fdcbf05773ec743d7a
MKSonny <minson9@naver.com>
2023-10-30 16:30:56 +0900

    알고리즘) 분할 정복: 색종이 만들기 해결

--- 백준/알고리즘 과제/분할 정복/Moo 게임.py
+++ 백준/알고리즘 과제/분할 정복/Moo 게임.py
@@ -4,4 +4,10 @@ S(1) = "m o o m o o o m o o"
 S(2) = "m o o m o o o m o o m o o o o m o o m o o o m o o"
 '''
 
+speak = "m00"
+
 def s(n):
+    if n == 0:
+        speak
+    else:
+        return s(n) + s(n - 1)
