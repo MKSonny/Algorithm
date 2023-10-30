9d5ecb8f659e353deb1f41266e4ae01c85deb725
MKSonny <minson9@naver.com>
2023-10-30 20:50:47 +0900

    알고리즘) 파일 정리

--- 백준/abc+cac=bdd.py
+++
@@ -1,6 +0,0 @@
-for a in range(1, 10):
-    for b in range(1, 10):
-        for c in range(1, 10):
-            for d in range(1, 10):
-                if (a * 100 + b * 10 + c) + c* 100+ a * 10 + c == b * 100 + d * 10 + d:
-                    print(a, b, c, d)
