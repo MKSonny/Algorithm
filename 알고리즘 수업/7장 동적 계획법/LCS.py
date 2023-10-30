d1a262013c1093fabe4887bcd1f2eff3f788a1ef
MKSonny <minson9@naver.com>
2023-10-30 20:49:20 +0900

    알고리즘) LCS 다시

--- 알고리즘 수업/7장 동적 계획법/LCS.py
+++ 알고리즘 수업/7장 동적 계획법/LCS.py
@@ -1,17 +1,40 @@
 string_a = list("HELLO WORLD")
 string_b = list("GAME OVER")
 
-table = [[0] * len(string_a) for _ in range(len(string_b))]
+def lcs_dp(x, y):
+    m = len(x)
+    n = len(y)
+    l = [[None] * (n + 1) for _ in range(m + 1)]
 
-for i in range(len(string_b)):
-    for j in range(len(string_a)):
-        if string_b[i] == string_a[j]:
-            # print('a')
-            table[i][j] = table[i - 1][j - 1] + 1
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                l[i][j] = 0
+            elif x[i - 1] == y[j - 1]:
+                l[i][j] = l[i - 1][j - 1] + 1
+            else:
+                l[i][j] = max(l[i - 1][j], l[i][j - 1])
+
+    return l[m][n]
+
+result = []
+
+def lcs_dp_traceback(x, y, l):
+    lcs = ""
+    i = len(x)
+    j = len(y)
+
+    while i > 0 and j > 0:
+        v = l[i][j]
+        if v > l[i][j - 1] and v > l[i - 1][j] and v > l[i - 1][j - 1]:
+            i -= 1
+            j -= 1
+            lcs = x[i] + lcs
+        elif v == l[i][j - 1] and v > [i - 1][j]:
+            j -= 1
         else:
-            table[i][j] = max(table[i - 1][j], table[i][j - 1])
+            i -= 1
+    return lcs
 
-for row in table:
-    print(row)
-
-table[len(string_b) - 1][len(string_a) -1]
+lcs = lcs_dp_traceback(string_a, string_b, table)
+print(lcs)
