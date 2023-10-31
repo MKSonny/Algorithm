ce7dd18a7a680919bacd71a6bb97b68b6527879a
MKSonny <minson9@naver.com>
2023-10-31 19:38:27 +0900

    알고리즘) 편집 거리 i, j의 관계 이해

--- 알고리즘 수업/7장 동적 계획법/편집 거리 순환 구조.py
+++ 알고리즘 수업/7장 동적 계획법/편집 거리 순환 구조.py
@@ -1,11 +1,27 @@
-def edit_distance(S, T, m, n):
+S = "tuesday"
+T = "thursday"
+m = len(S)
+n = len(T)
+
+mem = [[None for _ in range(n)] for _ in range(m)]
+
+def edit_distance_mem(S, T, m, n, mem):
     if m == 0:
         return n
     if n == 0:
         return m
-    if S[m - 1] == T[n - 1]:
-        return edit_distance(S, T, m - 1, n - 1)
 
-    return 1 + min(edit_distance(S, T, m, n - 1),  # 삽입
-                   edit_distance(S, T, m - 1, n),  # 삭제
-                   edit_distance(S, T, m - 1, n - 1))  # 대체
+    if mem[m - 1][n - 1] == None:
+        if S[m - 1] == T[n - 1]:
+            mem[m - 1][n - 1] = edit_distance_mem(S, T, m - 1, n - 1, mem)
+        else:
+            mem[m - 1][n - 1] = min(edit_distance_mem(S, T, m - 1, n - 1, mem),
+                                    edit_distance_mem(S, T, m, n - 1, mem),
+                                    edit_distance_mem(S, T, m - 1, n, mem)) + 1
+
+    return mem[m - 1][n - 1]
+
+print(edit_distance_mem(S, T, m, n, mem))
+
+for row in mem:
+    print(row)
