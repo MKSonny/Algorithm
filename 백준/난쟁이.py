0068bdd646f488c3a0e70aca5dbf7da4dfd57878
MKSonny <minson9@naver.com>
2023-11-01 19:08:48 +0900

    알고리즘) 편집 거리 i, j의 관계 이해

--- 백준/난쟁이.py
+++ 백준/난쟁이.py
@@ -7,6 +7,20 @@ for i in range(9):
 
 res = [0] * 7
 
+  # 2(치킨집) 를 리스트에 담아서 사이즈를 알수있다.
+  dfs(int inx , int count ){
+    for(int i =idx ; i< 2.size ; i++){
+        //1. 이리시트에서 인덱스를 빼서  x , y를 알 수 있다.
+        // 2. 선택한 경우
+            map[] 을 2로 냅둔다.
+         //  dfs ( i +1 , count + 1)
+            map[] = 0;
+    }
+}
+
+
+         10   2
+
 def dfs(l, s):
     if l == 7:
         total = 0
