8a15823ef61976c515d3b3c09dc1b38e7e1b3ca0
MKSonny <minson9@naver.com>
2023-11-03 20:50:54 +0900

    알고리즘) Dijkstra MST 셜명 부족, 이해 부족

--- 알고리즘 수업/8장 탐욕적 기법/Dijkstra MST.py
+++ 알고리즘 수업/8장 탐욕적 기법/Dijkstra MST.py
@@ -14,7 +14,6 @@ def getMinVertex(dist, selected):
     minv = -1
     mindist = INF
     for v in range(len(dist)):
-
         if not selected[v] and dist[v] < mindist:
             mindist = dist[v]
             minv = v
@@ -26,16 +25,31 @@ def shortest_path_dijkstra(vtx, adj, start):
     dist = list(adj[start])
     dist[start] = 0
     path = [start] * vsize
+    # visited
     found = [False] * vsize
     found[start] = True
+
+    # print('dist', dist)
 
     for i in range(vsize):
         print("Step%2d: " % (i + 1), dist)
         u = getMinVertex(dist, found)
+        # print('u', u)
+        # 가장 싼 비용 찾음
         found[u] = True
 
+        # u: 현재 처리 중인 노드(정점)입니다.
+        # u는 이전 단계에서 이미 방문한 노드 중에서 최단 경로를 가진 노드를 나타냅니다.
+        # 즉, 현재까지의 최단 경로가 발견된 노드입니다.
+
+        # w: 아직 방문하지 않은 노드 중에서 현재 처리 중인 노드 u와 연결된 노드를 나타냅니다.
+        # 즉, w는 u와 직접 연결된 노드 중에서 최단 경로를 찾고자 하는 노드입니다.
         for w in range(vsize):
-            if not found[w]:
+            # 가장 싼 비용이 아닌 것과 현재 시작 점이 아닌 것 중에서
+            if found[w] == False:
+                # dist[u] -> 현재 내가 찾은 가장 짧은 비용
+                # adj[u][w] -> 내가 찾은 가장 짧은 간선을 제외한 다른 간선들로 갔을 때의 경우들
+                # dist[w] ->
                 if dist[u] + adj[u][w] < dist[w]:
                     dist[w] = dist[u] + adj[u][w]
                     path[w] = u
@@ -46,11 +60,13 @@ print("Shortest Path By Dijkstra Algorithm")
 start = 0
 path = shortest_path_dijkstra(vertex, weight, start)
 
-for end in range(len(vertex)):
-    if end != start:
-        print("[최단경로: %s -> %s] %s" % (vertex[start], vertex[end], vertex[end]), end='')
-        while path[end] != start:
-            print(" <- %s" % vertex[path[end]], end='')
-            end = path[end]
+print('path', path)
 
-    print(" <- %s" % vertex[path[end]])
+# for end in range(len(vertex)):
+#     if end != start:
+#         print("[최단경로: %s -> %s] %s" % (vertex[start], vertex[end], vertex[end]), end='')
+#         while path[end] != start:
+#             print(" <- %s" % vertex[path[end]], end='')
+#             end = path[end]
+#
+#     print(" <- %s" % vertex[path[end]])
