37dd13a0c5138e9e041d960e963d09e9c81feaf2
MKSonny <minson9@naver.com>
2023-11-05 20:04:32 +0900

    알고리즘) 피보나치 수열 메모이제이션, 메모이제이션 동적 계획법 차이 이해 필요

--- 알고리즘 수업/7장 동적 계획법/피보나치 수열 메모이제이션.py
+++ 알고리즘 수업/7장 동적 계획법/피보나치 수열 메모이제이션.py
@@ -9,3 +9,13 @@ def fib_dp_mem(n):
 n = 7
 mem = [None] * (n + 1)
 print(fib_dp_mem(n))
+
+# 둘의 차이는?
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
