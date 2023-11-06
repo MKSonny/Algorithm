3fe4973ecfc702edacf33c60abc29e595991fea3
MKSonny <minson9@naver.com>
2023-11-06 16:02:06 +0900

    알고리즘) 백트래킹 미로 탐색 구현 성공

--- 알고리즘 수업/9장 백트래킹과 분기한정/미로 탐색.py
+++ 알고리즘 수업/9장 백트래킹과 분기한정/미로 탐색.py
@@ -1,40 +1,37 @@
-def isSafe(maze, x, y, mark):
+def is_safe(maze, x, y, mark):
     W, H = len(maze[0]), len(maze)
     if x >= 0 and x < W and y >= 0 and y < H:
         if maze[y][x] != 0 and mark[y][x] == 0:
             return True
         return False
 
-def solve_maze(maze, x, y):
+def find_way(maze, x, y):
     W, H = len(maze[0]), len(maze)
     sol = [[0 for _ in range(W)] for _ in range(H)]
     mark = [[0 for _ in range(W)] for _ in range(H)]
-
-    if DFS_maze(maze, x, y, sol, mark) == False:
-        print("출구를 찾을 수 없음")
+    if dfs(maze, x, y, sol, mark) == False:
+        print("길을 찾을 수 없습니다.")
     else:
         for i in sol:
             print(i)
 
-def DFS_maze(maze, x, y, sol, mark):
-    W, H = len(maze[0]), len(maze)
-
-    if not isSafe(maze, x, y, mark):
+def dfs(maze, x, y, sol, mark):
+    if not is_safe(maze, x, y, mark):
         return False
 
-    mark[y][x] = 1
     sol[y][x] = 1
+    mark[y][x] = 1
 
     if maze[y][x] == 2:
         return True
 
-    if DFS_maze(maze, x + 1, y, sol, mark):
+    if dfs(maze, x + 1, y, sol, mark):
         return True
-    if DFS_maze(maze, x - 1, y, sol, mark):
+    if dfs(maze, x - 1, y, sol, mark):
         return True
-    if DFS_maze(maze, x, y + 1, sol, mark):
+    if dfs(maze, x, y + 1, sol, mark):
         return True
-    if DFS_maze(maze, x, y - 1, sol, mark):
+    if dfs(maze, x, y - 1, sol, mark):
         return True
 
     sol[y][x] = 0
@@ -52,4 +49,4 @@ maze = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
-solve_maze(maze, 3, 0)
+find_way(maze, 3, 0)
