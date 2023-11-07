c7dbcc9ba3220e31fb82712a075fead1ff2afd6b
MKSonny <minson9@naver.com>
2023-11-07 19:18:32 +0900

    알고리즘) greedy 거스름돈 추가

---
+++ 알고리즘 수업/8장 탐욕적 기법/거스름돈 최소화.py
@@ -0,0 +1,16 @@
+coins = [500, 100, 50, 10, 5, 1]
+V = 580
+
+def min_coins_greedy(coins, V):
+    count = []
+    for i in range(len(coins)):
+        cnt = V // coins[i]
+        count.append(cnt)
+        V -= cnt * coins[i]
+
+    return count
+
+
+print("잔돈 액수 =", V)
+print("동전 종류 =", coins)
+print("동전 개수 =", min_coins_greedy(coins, V))
