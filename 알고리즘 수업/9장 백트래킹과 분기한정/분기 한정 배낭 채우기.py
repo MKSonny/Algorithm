c4e82e69c391af0f97a402c9e65663f1c57d0d65
MKSonny <minson9@naver.com>
2023-11-09 15:35:12 +0900

    알고리즘) 백트래킹 분기 한정 배낭 살짝 이해

--- 알고리즘 수업/9장 백트래킹과 분기한정/분기 한정 배낭 채우기.py
+++ 알고리즘 수업/9장 백트래킹과 분기한정/분기 한정 배낭 채우기.py
@@ -4,7 +4,7 @@ def bound(obj, W, level, weight, profit):
 
     pBound = profit
     for j in range(level + 1, len(obj)):
-        pBound += obj[j][i]
+        pBound += obj[j][1]
 
     return pBound
 
@@ -12,7 +12,6 @@ def knapSack_bnb(obj, W, level, weight, profit, maxProfit):
     if level == len(obj):
         return maxProfit
 
-    #
     if weight + obj[level][0] <= W:
         newWeight = weight + obj[level][0]
         newProfit = profit + obj[level][1]
@@ -21,18 +20,18 @@ def knapSack_bnb(obj, W, level, weight, profit, maxProfit):
 
         newBound = bound(obj, W, level, newWeight, newProfit)
         if newBound >= maxProfit:
-            maxProfit = knapSack_bnb(obj, W, level, weight, profit, maxProfit)
+            maxProfit = knapSack_bnb(obj, W, level + 1, newWeight, newProfit, maxProfit)
 
 
     newWeight = weight
     newProfit = profit
     newBound = bound(obj, W, level, newWeight, newProfit)
     if newBound >= maxProfit:
-        maxProfit = knapSack_bnb(obj, W, level, weight, profit, maxProfit)
+        maxProfit = knapSack_bnb(obj, W, level + 1, newWeight, newProfit, maxProfit)
 
 
     return maxProfit
 
-obj = [(2.5, 30, 'A'), (3.2, 50, 'B'), (1.7, 70, 'C'), (50, 60, 'D'), (4.1, 40, 'E'), ]
+obj = [(2.5, 30, 'A'), (3.2, 50, 'B'), (1.7, 70, 'C'), (5, 60, 'D'), (4.1, 40, 'E'), ]
 print(obj)
-print("0-1 배낭문제(분기 한정):", knapSack_bnb(obj, 10))
+print("0-1 배낭문제(분기 한정):", knapSack_bnb(obj, 10, 0, 0, 0, 0))
