cd902d7e7deb3d513e44e905a278cb197b32bd12
MKSonny <minson9@naver.com>
2023-11-20 13:37:58 +0900

    알고리즘) dp lcs traceback에서 same = X[i] + same 위치 조심

---
+++ 알고리즘 수업/7장 동적 계획법/복습/lcs4.py
@@ -0,0 +1,39 @@
+X = "GAME OVER"
+Y = "HELLO WORLD"
+print("X =", X)
+print("Y =", Y)
+
+def lcs_dp(X, Y):
+    m = len(X)
+    n = len(Y)
+
+    l = [[None] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                l[i][j] = 0
+            elif X[i - 1] == Y[j - 1]:
+                l[i][j] = l[i - 1][j - 1] + 1
+            else:
+                l[i][j] = max(l[i - 1][j], l[i][j - 1])
+    print(lcs_track(X, Y, l))
+    return l[m][n]
+
+def lcs_track(X, Y, l):
+    i = len(X)
+    j = len(Y)
+    same = ""
+    while i >= 0 and j >= 0:
+        v = l[i][j]
+        if v > l[i - 1][j - 1] and v > l[i - 1][j] and v > l[i][j - 1]:
+            i -= 1
+            j -= 1
+            same = X[i] + same
+        elif v == l[i][j - 1] and v > l[i - 1][j]:
+            j -= 1
+        else:
+            i -= 1
+    return same
+
+print("LCS(분할 정복)", lcs_dp(X, Y))
