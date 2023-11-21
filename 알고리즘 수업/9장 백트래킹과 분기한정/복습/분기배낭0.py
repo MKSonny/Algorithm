e118b3f9b12aff9d3b2b64a49af83232f0571be5
MKSonny <minson9@naver.com>
2023-11-21 18:52:52 +0900

    알고리즘) 백트래킹> 분기한정 배낭 채우기 완료

--- 알고리즘 수업/9장 백트래킹과 분기한정/복습/분기배낭0.py
+++ 알고리즘 수업/9장 백트래킹과 분기한정/복습/분기배낭0.py
@@ -1,7 +1,7 @@
 obj = [(2.5, 30, 'A'), (3.2, 50, 'B'), (1.7, 70, 'C'), (5, 60, 'D'), (4.1, 40, 'E'), ]
 W = 10
 
-def bound(obj, W, weight, level, profit):
+def bound(obj, W, weight, profit, level):
     if weight > W:
         return
 
@@ -12,18 +12,29 @@ def bound(obj, W, weight, level, profit):
 
     return pBound
 
-def knapsack(obj, W, weight, level, profit, maxProfit):
+def knapsack(obj, W, weight, profit, level, maxProfit):
     if level == len(obj):
         return maxProfit
 
     if weight + obj[level][0] <= W:
+        newWeight = weight + obj[level][0]
         newProfit = profit + obj[level][1]
-        newWeight = profit + obj[level][0]
 
         if newProfit > maxProfit:
             maxProfit = newProfit
 
-        newBound = bound(obj, W, newWeight, level, newProfit)
+        newBound = bound(obj, W, weight, profit, level)
 
         if newBound >= maxProfit:
-            knapsack(obj, W, newWeight, level + 1, newProfit, maxProfit)
+            maxProfit = knapsack(obj, W, newWeight, newProfit, level + 1, maxProfit)
+
+    newWeight = weight
+    newProfit = profit
+
+    newBound = bound(obj, W, weight, profit, level)
+    if newBound >+ maxProfit:
+        maxProfit = knapsack(obj, W, newWeight, newProfit, level + 1, maxProfit)
+
+    return maxProfit
+
+print(knapsack(obj, W, 0, 0, 0, 0))
