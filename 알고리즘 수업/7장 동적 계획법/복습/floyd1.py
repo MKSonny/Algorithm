74a2291f83bf39ba28a16d00d1bfc8be5e4635d4
MKSonny <minson9@naver.com>
2023-11-23 14:36:41 +0900

    알고리즘) dp> dijkstra 틀림

---
+++ 알고리즘 수업/7장 동적 계획법/복습/floyd1.py
@@ -0,0 +1,28 @@
+import copy
+
+INF = 9999
+vertex = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+weight = [
+    [0, 7, INF, INF, 3, 10, INF],
+    [7, 0, 4, 10, 2, 6, INF],
+    [INF, 4, 0, 2, INF, INF, INF],
+    [INF, 10, 2, 0, 11, 9, 4],
+    [3, 2, INF, 11, 0, 13, 5],
+    [10, 6, INF, 9, 13, 0, INF],
+    [INF, INF, INF, 4, 5, INF, 0]
+]
+
+def floyd(vertex, weight):
+    n = len(vertex)
+    dist = copy.deepcopy(weight)
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][k] + dist[k][j] < dist[i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+
+    for i in dist:
+        print(i)
+
+floyd(vertex, weight)
