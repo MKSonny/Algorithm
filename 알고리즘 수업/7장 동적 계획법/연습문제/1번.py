63d18dedfbf8f70972b4bd71ec3582e6cea233cf
MKSonny <minson9@naver.com>
2023-11-24 11:05:45 +0900

    알고리즘) dp> 연습문제> 1번 해결

--- 알고리즘 수업/7장 동적 계획법/연습문제/1번.py
+++ 알고리즘 수업/7장 동적 계획법/연습문제/1번.py
@@ -3,3 +3,26 @@
 두 가지 방법 으로 각각 구해 출력하는 프로그램을 작성하라. 
 '''
 
+def table_fib(n):
+    table = [None] * (n + 1)
+    table[0] = 0
+    table[1] = 1
+
+    for i in range(2, n + 1):
+        table[i] = table[i - 2] + table[i - 1]
+
+    return table[n]
+
+def do_table_mem(n):
+    mem = [None] * (n + 1)
+    return table_mem(n, mem)
+
+def table_mem(n, mem):
+    if mem[n] == None:
+        if n < 2:
+            mem[n] = n
+        else:
+            mem[n] = table_mem(n - 2, mem) + table_mem(n - 1, mem)
+    return mem[n]
+
+print(do_table_mem(7))
