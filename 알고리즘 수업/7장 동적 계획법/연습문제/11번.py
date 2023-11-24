87943b509be281c810fe336da91a1054cd1993b4
MKSonny <minson9@naver.com>
2023-11-24 11:42:36 +0900

    알고리즘) dp> 연습문제> 11번 미해결

--- 알고리즘 수업/7장 동적 계획법/연습문제/11번.py
+++ 알고리즘 수업/7장 동적 계획법/연습문제/11번.py
@@ -1,46 +1,23 @@
-value = [26, 20, 14, 40, 50]
-weight = [3, 2, 1, 4, 5]
+val = [26, 20, 14, 40, 50]
+wt = [3, 2, 1, 4, 5]
 W = 6
+n = len(val)
 
-C = [[None for _ in range(W + 1)] for _ in range(len(value) + 1)]
-
-def knapSack_mem(W, wt, val, n):
-    if C[n][W] == None:
-        if n == 0 or W == 0:
-            C[n][W] = 0
-    else:
-        if wt[n - 1] > W:
-            return C[n - 1][W]
+def knapsack_mem(W, val, wt, n, mem):
+    if mem[n][W] == None:
+        if W == 0 or n == 0:
+            mem[n][W] = 0
+        elif wt[n - 1] > W:
+            mem[n][W] = knapsack_mem(W, val, wt, n - 1, mem)
         else:
-            valWithout = knapSack_bf(W, wt, val, n - 1)
-            valWith = knapSack_bf(W - wt[n - 1], wt, val, n - 1) + val[n - 1]
+            valWith = val[n - 1] + knapsack_mem(W - wt[n - 1], val, wt, n - 1, mem)
+            valWithout = knapsack_mem(W, val, wt, n - 1, mem)
             return max(valWith, valWithout)
 
+    return mem[n][W]
 
-def knapSack_bf(W, wt, val, n):
-    if n == 0 or W == 0:
-        return 0
+def do_knapsack(W, val, n, wt):
+    mem = [[None for _ in range(W + 1)] for _ in range(n + 1)]
+    return knapsack_mem(W, val, wt, n, mem)
 
-    if wt[n - 1] > W:
-        return knapSack_bf(W, wt, val, n - 1)
-    else:
-        valWithout = knapSack_bf(W, wt, val, n - 1)
-        valWith = knapSack_bf(W - wt[n - 1], wt, val, n - 1) + val[n - 1]
-        return max(valWith, valWithout)
-
-
-def knapsack_dp(value, weight, W):
-    a = [[0 for _ in range(W + 1)] for _ in range(len(value) + 1)]
-
-    for i in range(1, len(value) + 1):
-        for w in range(1, W + 1):
-            if weight[i - 1] > w:
-                a[i][w] = a[i - 1][w]
-            else:
-                valueWith = a[i - 1][w - weight[i - 1]] + value[i - 1]
-                valueWithout = a[i - 1][w]
-                a[i][w] = max(valueWith, valueWithout)
-
-    return a[len(value)][W]
-
-print(knapsack_dp(value, weight, W))
+print(do_knapsack(W, val, n, wt))
