130e696a9f6e15f0a219701ead46f8656abc82bf
MKSonny <minson9@naver.com>
2023-11-24 11:12:22 +0900

    알고리즘) dp> 연습문제> 4번 해결

--- 알고리즘 수업/7장 동적 계획법/연습문제/4번.py
+++ 알고리즘 수업/7장 동적 계획법/연습문제/4번.py
@@ -1,21 +1,14 @@
-def bino_coef_dp(n, k):
-    C = [[-1 for _ in range(k + 1)] for _ in range(n + 1)]
+def bino_coef(n, k, mem):
+    if mem[n][k] == None:
+        if n == k or k == 0:
+            mem[n][k] = 1
+        else:
+            mem[n][k] = bino_coef(n - 1, k - 1, mem) + bino_coef(n - 1, k, mem)
+    return mem[n][k]
 
-    for i in range(n + 1):
-        for j in range(min(i, k) + 1):
-            if j == 0 or j == i:
-                C[i][j] = 1
-            else:
-                C[i][j] = C[i - 1][j - 1] + C[i - 1][j]
+def do_bino_coef(n, k):
+    mem = [[None for _ in range(k + 1)] for _ in range(n + 1)]
 
-    return C[n][k]
+    return bino_coef(n, k, mem)
 
-def bino_coef_mem(n, k):
-    if C[n][k] == None :
-        if k == 0 or k == n:
-            C[n][k] = 1
-    else :
-        C[n][k] = bino_coef_mem(n-1 , k-1) + bino_coef_mem(n-1 , k)
-    return C[n][k]
-
-print(bino_coef_dp(6, 3))
+print(do_bino_coef(4, 2))
