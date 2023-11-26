c9a8bb5bcf89aa29ba43b8c08ef4e1227c8c0240
MKSonny <minson9@naver.com>
2023-11-26 13:27:34 +0900

    알고리즘) greedy> 연습문제 6번, 12번> prim과 kruskal 결과 다름!

--- 알고리즘 수업/8장 탐욕적 기법/복습/all.py
+++ 알고리즘 수업/8장 탐욕적 기법/복습/all.py
@@ -1,13 +1,13 @@
 INF = 9999
-vertex = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+vertex = ['A', 'B', 'C', 'D', 'E']
 weight = [
-    [None, 29, None, None, None, 10, None],
-    [29, None, 16, None, None, None, 15],
-    [None, 16, None, 12, None, None, None],
-    [None, None, 12, None, 22, None, 18],
-    [None, None, None, 22, None, 27, 25],
-    [10, None, None, None, 27, None, None],
-    [None, 15, None, 18, 25, None, None]
+   # A  B  C  D  E
+    [None, 7, 4, None, 2],
+    [7, None, None, 3, 4],
+    [4, None, None, 6, 3],
+    [None, 3, 6, None, 5],
+    # [2, 4, 3, 5, None]
+    [2, 4, 3, 5, None]
 ]
 
 def getMinVertex(dist, selected):
@@ -32,42 +32,8 @@ def prim(vertex, weight):
             if weight[u][j] != None:
                 if not selected[j] and weight[u][j] < dist[j]:
                     dist[j] = weight[u][j]
+        # print("dist: %s =" % vertex[u], dist)
 
     print(dist)
 
 prim(vertex, weight)
-
-class disJoint:
-    def __init__(self, n):
-        self.parent = [-1] * n
-        self.set_size = n
-
-    def find(self, id):
-        while self.parent[id] >= 0:
-            id = self.parent[id]
-        return id
-
-    def union(self, s1, s2):
-        self.parent[s1] = s2
-        self.set_size = self.set_size - 1
-
-def kruskal(vertex, weight):
-    n = len(vertex)
-    set = disJoint()
-    E = []
-
-    for i in range(n - 1):
-        for j in range(i + 1, n):
-            if weight[i][j] != None:
-                E.append((i, j, weight[i][j]))
-
-    E.sort(key=lambda o:o[2])
-
-    for i in range(n):
-        e1 = E.pop()
-        e2 = E.pop()
-
-
-
-
-# def dijkstra():
