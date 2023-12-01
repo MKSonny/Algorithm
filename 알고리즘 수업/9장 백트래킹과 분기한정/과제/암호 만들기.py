d8e97a51c321cfac98c25f1ed9d9efe7d795f11f
MKSonny <minson9@naver.com>
2023-12-01 12:33:29 +0900

    알고리즘) backtracking> 과제> 암호 만들기 설명 추가

--- 알고리즘 수업/9장 백트래킹과 분기한정/과제/암호 만들기.py
+++ 알고리즘 수업/9장 백트래킹과 분기한정/과제/암호 만들기.py
@@ -5,7 +5,14 @@ password.sort()
 # print(password)
 
 vowels = list('aeiou')
-
+'''
+설명
+수업시간에 배운 부분합과 유사한 문제다. 
+만약 solution의 길이가 k 즉, 4자리 수가 된다면 바로 비밀번호를 인쇄하는 것이 아니라
+문제의 조건 자음이 하나 이상 모음이 두 개이상을 만족하는지 검사하는 is_safe_password 함수를 통해
+만약 조건을 만족하지 않는다면 백트래킹하여 다음 조합을 검사하도록 한다.
+강의 시간의 부분집합 문제에서 dfs로 Combination을 구하는 방법과  N-Queen, 미로찾기 문제에서의 isSafe를 활용하는 문제다.
+'''
 def combination(obj, level, k, sol):
     if len(sol) == k:
         if is_safe_password(sol):
