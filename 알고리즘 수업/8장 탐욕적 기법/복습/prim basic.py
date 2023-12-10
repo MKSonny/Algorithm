6c802b7bd580409556a5da813288ea40c53b67c2
MKSonny <minson9@naver.com>
2023-12-10 18:14:38 +0900

    알고리즘) greedy > prim 완성

--- 알고리즘 수업/8장 탐욕적 기법/복습/prim basic.py
+++ 알고리즘 수업/8장 탐욕적 기법/복습/prim basic.py
@@ -9,31 +9,3 @@ weight = [
     [10, None, None, None, 27, None, None],
     [None, 15, None, 18, 25, None, None]
 ]
-
-def getMinVertex(dist, selected):
-    min = INF
-    min_v = -1
-    for i in range(len(dist)):
-        if dist[i] < min and not selected[i]:
-            min = dist[i]
-            min_v = i
-    return min_v
-
-def prim(vertex, weight):
-    n = len(vertex)
-    dist = [INF] * n
-    selected = [False] * n
-    dist[0] = 0
-
-    for i in range(n):
-        u = getMinVertex(dist, selected)
-        selected[u] = True
-        print("%s selected :" % vertex[u], end=' ')
-        for j in range(n):
-            if weight[u][j] != None:
-                if weight[u][j] < dist[j] and not selected[j]:
-                    dist[j] = weight[u][j]
-        print("dist =", dist)
-
-prim(vertex, weight)
-
---
+++ 알고리즘 수업/8장 탐욕적 기법/복습/prim4.py
@@ -0,0 +1,38 @@
+INF = 9999
+vertex = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+weight = [
+    [None, 29, None, None, None, 10, None],
+    [29, None, 16, None, None, None, 15],
+    [None, 16, None, 12, None, None, None],
+    [None, None, 12, None, 22, None, 18],
+    [None, None, None, 22, None, 27, 25],
+    [10, None, None, None, 27, None, None],
+    [None, 15, None, 18, 25, None, None]
+]
+
+def getMinVertex(dist, selected):
+    min = INF
+    min_v = -1
+    for i in range(len(dist)):
+        if dist[i] < min and not selected[i]:
+            min = dist[i]
+            min_v = i
+    return min_v
+
+def prim(vertex, weight):
+    n = len(vertex)
+    dist = [INF] * n
+    selected = [False] * n
+    dist[0] = 0
+
+    for i in range(n):
+        u = getMinVertex(dist, selected)
+        selected[u] = True
+        for j in range(n):
+            if weight[u][j] != None:
+                if weight[u][j] < dist[j] and not selected[j]:
+                    dist[j] = weight[u][j]
+
+    print(dist)
+
+prim(vertex, weight)
