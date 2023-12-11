d834e3aa9936199a0bd1e786af555bc6e5847534
MKSonny <minson9@naver.com>
2023-12-11 15:52:32 +0900

    알고리즘) dp > floyd 완성

---
+++ 알고리즘 수업/7장 동적 계획법/복습/floyd basic.py
@@ -0,0 +1,11 @@
+INF = 9999
+vertex = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+weight = [
+    [0, 7, INF, INF, 3, 10, INF],
+    [7, 0, 4, 10, 2, 6, INF],
+    [INF, 4, 0, 2, INF, INF, INF],
+    [INF, 10, 2, 0, 11, 9, 4],
+    [3, 2, INF, 11, 0, 13, 5],
+    [10, 6, INF, 9, 13, 0, INF],
+    [INF, INF, INF, 4, 5, INF, 0]
+]
