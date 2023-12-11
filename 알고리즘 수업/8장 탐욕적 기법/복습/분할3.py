547a5663c548cb00dcd02d080f8610b0acc881d2
MKSonny <minson9@naver.com>
2023-12-11 16:08:45 +0900

    알고리즘) greedy > 분할 가방 완성

---
+++ 알고리즘 수업/8장 탐욕적 기법/복습/분할3.py
@@ -0,0 +1,21 @@
+obj = [('A', 10, 80), ('B', 12, 120), ('C', 8, 60)]
+W = 18
+
+def knapsack(obj, W):
+    obj.sort(key = lambda o:o[2]/o[1], reverse = True)
+    totalValue = 0
+
+    for o in obj:
+        if W <= 0:
+            break
+        if W - o[1] >= 0:
+            W -= o[1]
+            totalValue += o[2]
+        else:
+            fraction = W / o[1]
+            totalValue += fraction * o[2]
+            W = int(W - fraction * o[1])
+
+    print(totalValue)
+
+knapsack(obj, W)
