61400e050fef789e25fc04e5b3ce3cd4fdf6d1ea
MKSonny <minson9@naver.com>
2023-12-11 16:12:25 +0900

    알고리즘) greedy > heapq 문법 주의

--- 알고리즘 수업/8장 탐욕적 기법/복습/허프.py
+++ 알고리즘 수업/8장 탐욕적 기법/복습/허프.py
@@ -4,17 +4,15 @@ label = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 freq = [24, 3, 8, 10, 33, 6, 4, 12]
 
 def huffman(label, freq):
-    n = len(freq)
     h = []
-
-    for i in range(n):
+    for i in range(len(label)):
         heapq.heappush(h, (freq[i], label[i]))
 
-    for i in range(1, n):
+    for i in range(len(label) - 1):
         e1 = heapq.heappop(h)
         e2 = heapq.heappop(h)
         heapq.heappush(h, (e1[0] + e2[0], e1[1] + e2[1]))
-        print(e1, "+", e2)
+        print(e1, '+', e2)
 
     print(heapq.heappop(h))
 
