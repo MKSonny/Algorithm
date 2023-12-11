9251afec4503eed6067191cdd24bae887a03dda8
MKSonny <minson9@naver.com>
2023-12-11 16:05:36 +0900

    알고리즘) greedy > 거스름 돈 완성

--- 알고리즘 수업/8장 탐욕적 기법/복습/거스름돈.py
+++ 알고리즘 수업/8장 탐욕적 기법/복습/거스름돈.py
@@ -1,12 +1,14 @@
 coins = [500, 100, 50, 10, 5, 1]
 V = 580
 
-def calc_coins(coins, V):
+def calc(coins, V):
     count = []
+
     for i in range(len(coins)):
         cnt = V // coins[i]
         count.append(cnt)
-        V -= coins[i] * cnt
-    return count
+        V -= cnt * coins[i]
 
-print(calc_coins(coins, V))
+    print(count)
+
+calc(coins, V)
