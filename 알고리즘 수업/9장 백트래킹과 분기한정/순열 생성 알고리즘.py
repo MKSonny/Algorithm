eed7595dbb75bb0f76cef7bbf0bb4f6d80f2aa62
MKSonny <minson9@naver.com>
2023-12-15 21:50:58 +0900

    알고리즘) greedy > dijkstra, prim, kruskal

--- 알고리즘 수업/9장 백트래킹과 분기한정/순열 생성 알고리즘.py
+++ 알고리즘 수업/9장 백트래킹과 분기한정/순열 생성 알고리즘.py
@@ -2,7 +2,7 @@ def all_permutations(data):
     bUsed = [False] * len(data)
     DFS_permutation(data, [], 0, bUsed)
 
-#
+#ㅓ
 def DFS_permutation(data, sol, level, bUsed):
     if level == len(data):
         print(sol)
