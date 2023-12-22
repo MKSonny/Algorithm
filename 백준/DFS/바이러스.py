a19309dd873fdf8b07eb4031aeb075904cbc7d91
MKSonny <minson9@naver.com>
2023-12-22 14:20:47 +0900

    백준) DFS > 바이러스 성공 근데 global 사용 -> visited를 True가 아닌 0, 1로 표현하여 sum(visited) - 1로 해결

--- 백준/DFS/바이러스.py
+++ 백준/DFS/바이러스.py
@@ -6,7 +6,7 @@ n = int(input())
 m = int(input())
 
 node = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
+visited = [0] * (n + 1)
 
 for _ in range(m):
     start, end = map(int, input().split())
@@ -19,13 +19,13 @@ for _ in range(m):
 cnt = 0
 
 def dfs(starting):
-    global cnt
-    visited[starting] = True
+    # global cnt
+    visited[starting] = 1
     for key in node[starting]:
-        if not visited[key]:
-            cnt += 1
-            visited[key] = True
+        if visited[key] == 0:
+            # cnt += 1
+            visited[key] = 1
             dfs(key)
 
 dfs(1)
-print(cnt)
+print(sum(visited) - 1)
