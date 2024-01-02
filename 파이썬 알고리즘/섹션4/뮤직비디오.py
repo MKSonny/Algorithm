09438bdabe40ca384ff2e6f7dea5be89f70044d7
MKSonny <minson9@naver.com>
2024-01-02 15:20:02 +0900

    인프런 알고리즘) 결정알고리즘 > 뮤직비디오 해당 코드는 9, 9일때 반례 존재

---
+++ 파이썬 알고리즘/섹션4/뮤직비디오.py
@@ -0,0 +1,40 @@
+n, m = map(int, input().split())
+l = list(map(int, input().split()))
+
+'''
+9 3
+1 2 3 4 5 6 7 8 9
+
+9 9
+1 2 3 4 5 6 7 8 9
+'''
+
+lt = l[0]
+rt = sum(l)
+
+def binary_cnt(l, lt, rt, key):
+    min = 0
+    while lt <= rt:
+        mid = (lt + rt) // 2
+
+        cnt = 0
+        total = 0
+
+        for k in l:
+            cnt += k
+            if cnt > mid:
+                total += 1
+                cnt = k
+
+        total += 1
+        # print(total)
+
+        if total <= key:
+            min = mid
+            rt = mid - 1
+        else:
+            lt = mid + 1
+
+    return min
+
+print(binary_cnt(l, lt, rt, m))
