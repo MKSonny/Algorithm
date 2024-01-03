adeda14cf36129b96bda1c01fa575b2d76920192
MKSonny <minson9@naver.com>
2024-01-03 19:54:04 +0900

    인프런 알고리즘) 동적계획법 > 알리바바와 40인의 도둑 구현 성공!!

---
+++ 파이썬 알고리즘/섹션8 동적계획법/알리바바와 40인의 도둑.py
@@ -0,0 +1,30 @@
+n = int(input())
+
+'''
+5
+3 7 2 1 9
+5 8 3 9 2
+5 3 1 2 3
+5 4 3 2 1
+1 7 5 2 4
+'''
+
+l = []
+for _ in range(n):
+    l.append(list(map(int, input().split())))
+
+sum_a = 0
+sum_b = 0
+
+for j in range(n):
+    sum_a += l[0][j]
+    sum_b += l[j][0]
+    l[0][j] = sum_a
+    l[j][0] = sum_b
+
+for i in range(1, n):
+    for j in range(1, n):
+        l[i][j] = min(l[i - 1][j] + l[i][j], l[i][j - 1] + l[i][j])
+
+for i in l:
+    print(i)
