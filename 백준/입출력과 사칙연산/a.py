23c29e077bdb754c79220a86338d541f57e442bd
MKSonny <minson9@naver.com>
2024-01-08 17:01:38 +0900

    인프런 알고리즘) 삼성 문제

--- 백준/입출력과 사칙연산/a.py
+++ 백준/입출력과 사칙연산/a.py
@@ -1,10 +1,9 @@
+'''
+Sprout
+3
+'''
+
+a = list(input())
 n = int(input())
-l = list(map(int, input().split()))
-m = int(input())
 
-cnt = 0
-for i in range(n):
-    if l[i] == m:
-        cnt += 1
-
-print(cnt)
+print(a[n - 1])
