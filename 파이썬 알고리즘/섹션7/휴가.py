35f360c7681d7457e0f7b85e7adcf5024c3377ea
MKSonny <minson9@naver.com>
2024-01-09 19:53:16 +0900

    인프런 알고리즘) DFS > 휴가 구현 성공

---
+++ 파이썬 알고리즘/섹션7/휴가.py
@@ -0,0 +1,36 @@
+n = int(input())
+
+l = []
+
+for i in range(n):
+    day, val = map(int, input().split())
+    l.append((day, val))
+
+
+def dfs(l, level, total):
+    if level >= n:
+        return total
+
+    day = l[level][0]
+    val = l[level][1]
+
+    total += val
+    return dfs(l, level + day, total)
+
+h = []
+
+for i in range(n):
+    h.append(dfs(l, i, 0))
+
+print(max(h))
+
+'''
+7
+4 20
+2 10
+3 15
+3 20
+2 30
+2 20
+1 10
+'''
