96d6ae6873d1ec3089866e4b841f368881df3517
MKSonny <minson9@naver.com>
2024-01-16 13:56:27 +0900

    인프런 알고리즘) 구현력 > k번째 큰 수 구현은 성공했지만 for문 이해 부족

--- 파이썬 알고리즘/섹션2/k번째 큰 수.py
+++ 파이썬 알고리즘/섹션2/k번째 큰 수.py
@@ -4,24 +4,25 @@ l = list(map(int, input().split()))
 10 3
 13 15 34 23 45 65 33 11 26 42
 '''
-l.sort(reverse=True)
 cnt = 0
 tmp = -1
 
 h = []
 
-for i in range(1, n - 1):
+for i in range(n):
     sum = 0
-    sum += l[i - 1] + l[i]
-    for j in range(2, n):
+    sum += l[i]
+    for j in range(i + 1, n):
         sum += l[j]
-        print(sum)
-        h.append(sum)
+        for k in range(j + 1, n):
+            sum += l[k]
+            h.append(sum)
+            sum -= l[k]
         sum -= l[j]
-
+    sum -= l[i]
 
 h.sort()
 h = list(set(h))
 h.sort()
-print(h)
+# print(h)
 print(h[-m])
