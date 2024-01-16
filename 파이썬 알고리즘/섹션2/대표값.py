9cf8adc3dbdcc3b7a63f99520bb42de204a269d0
MKSonny <minson9@naver.com>
2024-01-16 15:05:54 +0900

    인프런 알고리즘) 구현력 > 대표값 round 함수 문제 수정

--- 파이썬 알고리즘/섹션2/대표값.py
+++ 파이썬 알고리즘/섹션2/대표값.py
@@ -5,6 +5,12 @@ l = list(map(int, input().split()))
 45 73 66 87 92 67 75 79 75 80
 '''
 base = round(sum(l) / n)
+# 기존 round 방식은 round_half_even 방식을 택한다.
+# round_half_even 방식은 4.500일 경우
+# round(4.500)의 결과는 4.5가 나온다.
+# 따라서 a = 4.5이면 a += 0.5를 한 이후
+# a = int(a)로 반올림을 처리한다.
+
 h = []
 for value in l:
     h.append(abs(value - base))
