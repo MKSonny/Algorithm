fe5cf2507ddaa9b04fd7623557b93fd0e17652cf
MKSonny <minson9@naver.com>
2024-01-17 14:43:08 +0900

    인프런 알고리즘) 탐색 > 카트 역배치 구현 성공

---
+++ 파이썬 알고리즘/섹션3/카드 역배치.py
@@ -0,0 +1,16 @@
+l = [i + 1 for i in range(20)]
+
+def _do(l, n, m):
+    n -= 1
+    m -= 1
+    for i in range(abs(n - m) // 2 + 1):
+        tmp = l[n + i]
+        l[n + i] = l[m - i]
+        l[m - i] = tmp
+
+for _ in range(10):
+    n, m = map(int, input().split())
+    _do(l, n, m)
+
+for i in l:
+    print(i, end=' ')
