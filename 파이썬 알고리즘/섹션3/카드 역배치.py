4927eca438d3f28c4aa2899ce687666c9d4a06a6
MKSonny <minson9@naver.com>
2024-01-17 14:51:36 +0900

    인프런 알고리즘) 탐색 > 카트 역배치 구현 성공 -> 파이썬에서는 swap을 a, b = b, a처럼 간단하게 구현할 수 있다.

--- 파이썬 알고리즘/섹션3/카드 역배치.py
+++ 파이썬 알고리즘/섹션3/카드 역배치.py
@@ -4,9 +4,10 @@ def _do(l, n, m):
     n -= 1
     m -= 1
     for i in range(abs(n - m) // 2 + 1):
-        tmp = l[n + i]
-        l[n + i] = l[m - i]
-        l[m - i] = tmp
+        # tmp = l[n + i]
+        # l[n + i] = l[m - i]
+        # l[m - i] = tmp
+        l[n + i], l[m - i] = l[m - i], l[n + i]
 
 for _ in range(10):
     n, m = map(int, input().split())
