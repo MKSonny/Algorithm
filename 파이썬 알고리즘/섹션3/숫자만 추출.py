3763ec9e6af6b0fb172243f1003ac49594c4a868
MKSonny <minson9@naver.com>
2024-01-17 14:06:38 +0900

    인프런 알고리즘) 탐색 > 숫자만 추출 구현 성공

---
+++ 파이썬 알고리즘/섹션3/숫자만 추출.py
@@ -0,0 +1,18 @@
+l = list(input())
+t = 0
+
+for i in l:
+    if i.isdigit():
+        t = t * 10 + int(i)
+
+def _find(num):
+    cnt = 2
+
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            cnt += 1
+
+    return cnt
+
+print(t)
+print(_find(t))
