f953ae81f2cae3bed94816df7432be6cec387f35
MKSonny <minson9@naver.com>
2024-01-19 18:35:53 +0900

    인프런 알고리즘) 이진탐색 > lis 복습

---
+++ 백준/이진 탐색/LIS 복습.py
@@ -0,0 +1,24 @@
+n = int(input())
+l = list(map(int, input().split()))
+
+h = [l[0]]
+
+def binary_search(A, key, low, high):
+    if low > high:
+        return low
+    mid = (low + high) // 2
+
+    if key == mid:
+        return mid
+    elif key < A[mid]:
+        return binary_search(A, key, low, mid - 1)
+    else:
+        return binary_search(A, key, mid + 1, high)
+
+for i in range(1, len(l)):
+    if h[-1] < l[i]:
+        h.append(l[i])
+    else:
+        h[binary_search(h, l[i], 0, len(h) - 1)] = l[i]
+
+print(len(h))
