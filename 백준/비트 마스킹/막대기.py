b6462461f55ab9e450eebefefb8f1614ceadbd9f
MKSonny <minson9@naver.com>
2024-01-29 18:51:31 +0900

    백준) 비트 마스킹 > 막대기 이진수를 사용하여 해결 가능하다.

--- 백준/비트 마스킹/막대기.py
+++ 백준/비트 마스킹/막대기.py
@@ -1,14 +1,28 @@
+# n = int(input())
+#
+# x = 64
+# l = [64, 32, 16, 8, 4, 2, 1]
+#
+# cnt = 0
+# for i in l:
+#     if n - i < 0:
+#         continue
+#     else:
+#         n -= i
+#         cnt += 1
+#
+# print(cnt)
+
 n = int(input())
-
-x = 64
-l = [64, 32, 16, 8, 4, 2, 1]
-
+l = bin(n)
 cnt = 0
+
 for i in l:
-    if n - i < 0:
-        continue
-    else:
-        n -= i
+    # 항상 0b로 시작하기 때문에
+    # 아래와 같이해도 정답
+    # 0b에는 1이 없음
+    # print(i)
+    if i == '1':
         cnt += 1
 
 print(cnt)
