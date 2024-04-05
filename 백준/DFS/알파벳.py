5c86022f06d60e4a02feebc2d9ca89a7d9e40bc0
MKSonny <minson9@naver.com>
2024-04-05 14:14:26 +0900

    백준) dfs> 알파벳 기존 답 pypy3 로 해야 정답 처리 java로 가야되나..

--- 백준/DFS/알파벳.py
+++ 백준/DFS/알파벳.py
@@ -4,38 +4,30 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 l = []
+
 for _ in range(n):
     l.append(list(input().strip()))
 
-visited = [[False for _ in range(m)] for _ in range(n)]
+visited = [0] * 26
+visited[ord(l[0][0]) - 65] = True
 
 dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
 
-mark = set()
 
-def dfs(y, x, mark, sol):
-    global maxx
+def dfs(y, x, count):
+    global answer
+    answer = max(answer, count)
     for i in range(4):
         ny = y + dy[i]
         nx = x + dx[i]
         if 0 <= ny < n and 0 <= nx < m:
-            if l[ny][nx] not in mark and not visited[ny][nx]:
-                mark.add(l[ny][nx])
-                visited[ny][nx] = True
-                maxx += 1
+            if not visited[ord(l[ny][nx]) - 65]:
+                visited[ord(l[ny][nx]) - 65] = True
+                dfs(ny, nx, count + 1)
+                visited[ord(l[ny][nx]) - 65] = False
 
-                dfs(ny, nx, mark, sol)
-                mark.remove(l[ny][nx])
-                sol.append(maxx)
-                maxx -= 1
-                visited[ny][nx] = False
 
-sol = []
-maxx = 1
-visited[0][0] = True
-mark.add(l[0][0])
-
-dfs(0, 0, mark, sol)
-# print(sol)
-print(max(sol))
+answer = 1
+dfs(0, 0, 1)
+print(answer)
