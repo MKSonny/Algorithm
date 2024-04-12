0493ae1c52c0f57591a622464caf7e8fc03654f2
MKSonny <minson9@naver.com>
2024-04-12 19:11:17 +0900

    백준) 대기업 코테> 창고 다각형 정답 봐서 구현 성공

--- 백준/대기업 코테에서 나오는 유형 모음/창고 다각형.py
+++ 백준/대기업 코테에서 나오는 유형 모음/창고 다각형.py
@@ -2,32 +2,22 @@ import heapq
 import sys
 
 n = int(sys.stdin.readline())
-l = []
+graph = [0 for _ in range(1001)]
 
 for _ in range(n):
-    l.append(list(map(int, sys.stdin.readline().rstrip().split())))
+    x, y = map(int, sys.stdin.readline().rstrip().split())
+    graph[x] = y
 
-l.sort()
-temp = []
 
-maxx = -1
-def look_both_side(l, i):
-    global maxx
-    if i == 0 or i == (n - 1):
-        temp.append(l[i])
-        if maxx > l[i][1]:
-            print(l[i][1], i)
-        maxx = max(l[i][1], maxx)
-        return True
-    if l[i][1] > l[i - 1][1] and l[i][1] > l[i + 1][1]:
-        if maxx > l[i][1]:
-            print(l[i][1])
-        maxx = max(l[i][1], maxx)
-        temp.append(l[i])
-        return True
-    return False
-
-for i in range(n):
-    look_both_side(l, i)
-
-print(temp)
+a = graph.index(max(graph))
+answer = 0
+maxx = 0
+for i in range(a + 1):
+    maxx = max(maxx, graph[i])
+    answer += maxx
+# print(answer)
+maxx = 0
+for j in range(1000, a, -1):
+    maxx = max(maxx, graph[j])
+    answer += maxx
+print(answer)
