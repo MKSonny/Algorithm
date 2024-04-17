7dc4ed1f0e2d15a21fd2abc34b10c66b2793dc17
MKSonny <minson9@naver.com>
2024-04-17 18:26:05 +0900

    백준) 대기업 코테> 녹색 옷 입은 애가 젤다지? 실패

---
+++ 백준/대기업 코테에서 나오는 유형 모음/녹색 옷 입은 애가 젤다지?.py
@@ -0,0 +1,49 @@
+import copy
+import sys
+
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+def move(y, x, n, dist, visited):
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if 0 <= ny < n and 0 <= nx < n:
+            if not visited[ny][nx]:
+                dist[ny][nx] = l[ny][nx] + dist[y][x]
+                visited[ny][nx] = True
+            if l[ny][nx] + dist[y][x] < dist[ny][nx]:
+                dist[ny][nx] = l[ny][nx] + dist[y][x]
+
+    # print('a')
+    # for p in dist:
+    #     print(p)
+    # print()
+
+
+cnt = 0
+while True:
+    n = int(sys.stdin.readline())
+    if n == 0:
+        break
+
+    cnt += 1
+    l = []
+
+    visited = [[False for _ in range(n)] for _ in range(n)]
+
+    for _ in range(n):
+        l.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+    dist = copy.deepcopy(l)
+
+    for i in range(n):
+        for j in range(n):
+            visited[i][j] = True
+            move(i, j, n, dist, visited)
+
+    for p in dist:
+        print(p)
+    print()
+
+    print("Problem %d: %d" % (cnt, dist[n - 1][n - 1]))
