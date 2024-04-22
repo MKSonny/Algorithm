31fbe0a8a71f90e86b345b87aaee28f7229b77fc
MKSonny <minson9@naver.com>
2024-04-22 19:20:01 +0900

    프로그래머스) 연습문제 Lv2> 도넛과 막대 그래프 수정 실패

--- 프로그래머스/연습문제 Lv2/도넛과 막대 그래프.py
+++ 프로그래머스/연습문제 Lv2/도넛과 막대 그래프.py
@@ -1,20 +1,18 @@
-def dfs(l, node, node_cnt, edge_cnt, start_node, visited):
-    # if node == start_node and node_cnt > 0:
-    #     return node_cnt, edge_cnt
+import copy
 
-    if not visited[node]:
-        node_cnt += 1
-        visited[node] = True
-        # print(node)
 
-    for idx, i in enumerate(l[node]):
-        if i == 1:
-            # if node != start_node:
-            edge_cnt += 1
+def dfs(l, node, start_node, node_cnt, edge_cnt, moved):
+    # print('dfs node', node)
+    if start_node == node and moved == 1:
+        return node_cnt, edge_cnt
+    node_cnt += 1
+    for idx, item in enumerate(l[node]):
+        if item == 1:
             l[node][idx] = -1
-            return dfs(l, idx, node_cnt, edge_cnt, start_node, visited)
-
-
+            # print('a')
+            moved = 1
+            edge_cnt += 1
+            return dfs(l, idx, start_node, node_cnt, edge_cnt, moved)
     return node_cnt, edge_cnt
 
 
@@ -27,57 +25,14 @@ def solution(edges):
         a, b = edges[i]
         l[a][b] = 1
 
-    maxx = -1
-    max_i = -1
-
     for i in range(1, node + 1):
-        if maxx < l[i].count(1):
-            maxx = l[i].count(1)
-            max_i = i
-
-    # dfs(l, node, node_cnt, edge_cnt, start_node)
-    # for i in range(1, node + 1):
-
-    # print(l[max_i])
-
-    donut = 0
-    graph = 0
-    eight = 0
-
-    visited = [False] * (node + 1)
-    visited[0] = True
-
-    # while visited.count(True) != node:
-    q = []
-    for i in range(len(l[max_i])):
-        if l[max_i][i] == 1:
-            q.append(i)
-
-    # 중심과 연결된 간선의 연결을 모두 끊는다.
-    visited[max_i] = True
-
-
-    while q:
-        i = q.pop(0)
         print(i)
-        node_cnt, edge_cnt = dfs(l, i, 0, 0, i, visited)
-
-        print(node_cnt, edge_cnt)
-        if len(q) == 0 and visited.count(True) != node + 1:
-            temp = visited.index(False)
-            # print(l[temp])
-            for idx, i in enumerate(l[temp]):
-                if i == 1:
-                    # print(i, idx)
-                    visited[idx] = False
-            q.append(temp)
-
-
-    # print(l[max_i])
+        temp_l = copy.deepcopy(l)
+        print(dfs(temp_l, i, i, 0, 0, 0))
 
     return answer
 
 
-solution([[2, 3], [4, 3], [1, 1], [2, 1]])
-print()
+# solution([[2, 3], [4, 3], [1, 1], [2, 1]])
+# print()
 solution([[4, 11], [1, 12], [8, 3], [12, 7], [4, 2], [7, 11], [4, 8], [9, 6], [10, 11], [6, 10], [3, 5], [11, 1], [5, 3], [11, 9], [3, 8]])
