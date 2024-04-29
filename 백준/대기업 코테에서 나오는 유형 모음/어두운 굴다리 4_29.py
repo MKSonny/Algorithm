3cc77b93c7ffa68a06712b74f692730a3a3a8246
MKSonny <minson9@naver.com>
2024-04-29 18:35:03 +0900

    백준) 대기업 코테> 어두운 굴다리: 시간 초과 발생 -> 다른 방식으로 prev_start ~ prev_end 구간을 적는 방식으로 해도 시간초과 발생

--- 백준/대기업 코테에서 나오는 유형 모음/어두운 굴다리 4_29.py
+++ 백준/대기업 코테에서 나오는 유형 모음/어두운 굴다리 4_29.py
@@ -5,25 +5,27 @@ m = int(sys.stdin.readline())
 
 k = list(map(int, sys.stdin.readline().rstrip().split()))
 
-def is_safe(l):
-    for i in l:
-        if i == 0:
-            return False
-    return True
+min_i = -1
 
-for i in range(1, n + 1):
-    l = [0] * n
-    for key in k:
-        s = key - i
-        e = key + i
+for i in range(n + 1, 1, -1):
+    for key in range(m):
+        s = k[key] - i
+        e = k[key] + i
         if s < 0:
             s = 0
         if e >= n:
             e = n
-        for j in range(s, e):
-            if l[j] == 1:
-                continue
-            l[j] = 1
-    if is_safe(l):
-        print(i)
+
+        if key == 0:
+            prev_s = s
+            prev_e = e
+        else:
+            if prev_e >= s:
+                prev_e = e
+
+    if prev_s == 0 and prev_e == n:
+        min_i = i
+    else:
         break
+
+print(min_i)
