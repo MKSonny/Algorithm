806c234b9427a07b58845826052f21a13db04bcb
MKSonny <minson9@naver.com>
2024-04-30 13:38:32 +0900

    프로그래머스> 연습문제 Lv3> 기지국 설치: N의 범위가 크다고 무조건 이진탐색을 사용하지 않음, 수학적인 사고도 필요

---
+++ 프로그래머스/연습문제 Lv3/기지국 설치.py
@@ -0,0 +1,28 @@
+def solution(n, stations, w):
+    answer = 0
+    std = w * 2 + 1
+
+    cnt = 0
+    start = 0
+
+    for station in stations:
+        s = station - w
+        e = station + w
+        if s < 0:
+            s = 0
+        if e > n:
+            e = n
+        cnt = s - start - 1
+        # print(s, start)
+        start = e
+        answer += cnt // std
+        if cnt % std != 0:
+            answer += 1
+
+    if e != n:
+        cnt = n - e
+        answer += cnt // std
+        if cnt % std != 0:
+            answer += 1
+
+    return answer
