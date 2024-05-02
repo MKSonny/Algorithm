fa2e53bfa6bb13c7c90ac4a72ac21864b059c03f
MKSonny <minson9@naver.com>
2024-05-02 15:54:46 +0900

    프로그래머스) 연습문제 Lv2> 게임 맵 최단 거리: 큐에서 (y, x)좌표를 꺼낼 때 항상 맨 앞 부터 꺼내야 한다. list로 구현해서 실수함

---
+++ 알고리즘 수업/9장 백트래킹과 분기한정/연습문제/게임 맵 최단거리.py
@@ -0,0 +1,50 @@
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+
+# cnt = 1
+
+def solution(maps):
+    answer = 0
+
+    #     for i in maps:
+    #         print(i)
+    #     print()
+
+    W = len(maps[0])
+    H = len(maps)
+
+    visited = [[False for _ in range(W)] for _ in range(H)]
+    visited[0][0] = True
+
+    # if maps[H - 1 - 1][W - 1] == 0 and maps[H - 1][W - 1 - 1] == 0
+
+    def print_map(maps):
+        for i in maps:
+            print(i)
+        print()
+
+    def bfs(maps, y, x):
+        # global cnt
+        q = []
+        q.append((y, x))
+        while q:
+            y, x = q.pop(0)
+            for i in range(4):
+                ny = y + dy[i]
+                nx = x + dx[i]
+                if 0 <= ny < H and 0 <= nx < W:
+                    if not visited[ny][nx] and maps[ny][nx] >= 1:
+                        maps[ny][nx] = maps[y][x] + 1
+                        visited[ny][nx] = True
+                        # print_map(maps)
+                        q.append((ny, nx))
+
+    bfs(maps, 0, 0)
+
+    if maps[H - 1][W - 1] == 1:
+        return -1
+    else:
+        return maps[H - 1][W - 1]
+
+    return answer
