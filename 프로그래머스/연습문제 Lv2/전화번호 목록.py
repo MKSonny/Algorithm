d20be3ae5f70abd058f5b7c2862e981ba728fc0b
MKSonny <minson9@naver.com>
2024-05-02 14:03:02 +0900

    프로그래머스) 연습문제 Lv2> 전화번호 목록: 시간 초과 발생 -> Counter 활용 시간 초과 발생

--- 프로그래머스/연습문제 Lv2/전화번호 목록.py
+++ 프로그래머스/연습문제 Lv2/전화번호 목록.py
@@ -1,19 +1,23 @@
+from collections import Counter
+
+
 def solution(phone_book):
     n = len(phone_book)
+
+    phone_book.sort(key=lambda o: len(o))
+
     for i in range(n - 1):
         for j in range(i + 1, n):
+            str_1_cnt = Counter(phone_book[i])
+            str_2_cnt = Counter(phone_book[j])
+            if len(str_1_cnt - str_2_cnt) > 0:
+                continue
             str_1 = phone_book[i]
             str_2 = phone_book[j]
-            fp = 0
-            sp = 0
-            while sp < len(str_2):
-                if str_1[fp] == str_2[sp]:
-                    fp += 1
-                    sp += 1
-                    if fp == len(str_1):
-                        return False
-                else:
-                    sp += 1
-
+            n_1 = len(str_1)
+            n_2 = len(str_2)
+            for k in range(n_2 - n_1 + 1):
+                if str_2[i:i + n_1] == str_1:
+                    return False
     answer = True
     return answer
