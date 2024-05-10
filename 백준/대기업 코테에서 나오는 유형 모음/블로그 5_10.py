9e826489397efc8cc869df7f2af5596523df4ce7
MKSonny <minson9@naver.com>
2024-05-10 13:23:08 +0900

    백준) 대기업 코테> 블로그: 시간 초과 발생 -> 파이썬 슬라이싱은 시간 초과가 발생할 수 있음, 스택을 활용해 시간 단축

--- 백준/대기업 코테에서 나오는 유형 모음/블로그 5_10.py
+++ 백준/대기업 코테에서 나오는 유형 모음/블로그 5_10.py
@@ -1,15 +1,40 @@
 import sys
+from collections import deque
 
 n, m = map(int, sys.stdin.readline().split())
 l = list(map(int, sys.stdin.readline().split()))
-
+'''
+5 1
+0 0 1 0 0
+'''
 maxx = -1
-cnt = 1
-for i in range(n - m + 1):
-    a = sum(l[i:i + m])
-    if maxx == a:
-        cnt += 1
-    maxx = max(maxx, a)
+cnt = 0
+stack = deque()
+total = 0
+for i in range(n):
+    total += l[i]
+    stack.append(l[i])
+    if len(stack) > m:
+        item = stack.popleft()
+        total -= item
+    if len(stack) == m:
+        if maxx <= total:
+            # cnt += 1
+            maxx = total
+        # maxx = max(total, maxx)
+
+cnt = 0
+stack = deque()
+total = 0
+for i in range(n):
+    total += l[i]
+    stack.append(l[i])
+    if len(stack) > m:
+        item = stack.popleft()
+        total -= item
+    if len(stack) == m:
+        if maxx == total:
+            cnt += 1
 
 if maxx == 0:
     print("SAD")
