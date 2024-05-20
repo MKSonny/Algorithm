8f7aff9575873d070af612eb17e14fb059f3f8af
MKSonny <minson9@naver.com>
2024-05-20 14:36:50 +0900

    백준) 대기업 코테> 비슷한 단어: Counter 사용하여 구현 성공

--- 백준/대기업 코테에서 나오는 유형 모음/비슷한 단어.py
+++ 백준/대기업 코테에서 나오는 유형 모음/비슷한 단어.py
@@ -1,16 +1,32 @@
 import sys
+from collections import Counter
 
 n = int(sys.stdin.readline())
 l = []
+ori = []
 for _ in range(n):
-    a = list(sys.stdin.readline().rstrip())
-    a.sort()
-    l.append(set(a))
+    a = sys.stdin.readline().rstrip()
+    ori.append(a)
+    l.append(Counter(a))
 
 cnt = 0
 
 for i in range(1, n):
-    if l[0] == l[i]:
-        cnt += 1
+    if abs(len(ori[0]) - len(ori[i])) > 1:
+        continue
+    a = l[i] - l[0]
+    b = l[0] - l[i]
+    if len(a) < 2 and len(b) < 2:
+        if sum(a.values()) < 2 and sum(b.values()) < 2:
+            cnt += 1
 
 print(cnt)
+'''
+6
+ABAAC
+ABAA
+ABAC
+ABAAA
+ABCAA
+ABSAC
+'''
