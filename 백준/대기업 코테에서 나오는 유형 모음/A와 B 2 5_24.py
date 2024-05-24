2f43d7e28c7e7ac5e0a783adeebc0fbb6b8216d6
MKSonny <minson9@naver.com>
2024-05-24 14:30:52 +0900

    백준) 대기업 코테> A와 B 2: 정답 보고 구현

--- 백준/대기업 코테에서 나오는 유형 모음/A와 B 2 5_24.py
+++ 백준/대기업 코테에서 나오는 유형 모음/A와 B 2 5_24.py
@@ -1,42 +1,18 @@
 import sys
 
-# 13:5
-
 s = list(sys.stdin.readline().rstrip())
 t = list(sys.stdin.readline().rstrip())
 
-s_idx = 0
-t_idx = 0
+def dfs(t):
+    if t == s:
+        print(1)
+        exit()
+    if len(t) == 0:
+        return 0
+    if t[-1] == 'A':
+        dfs(t[:-1])
+    if t[0] == 'B':
+        dfs(t[1:][::-1])
 
-if t == s:
-    print(1)
-    exit()
-
-flag = False
-
-while t_idx < len(t):
-    if s[s_idx] != t[t_idx] or flag:
-        if t[t_idx] == 'B':
-            if s_idx + 1 < len(s) and s[s_idx + 1] == 'B':
-                s.reverse()
-            else:
-                s.append('B')
-                s.reverse()
-        else:
-            s.append('A')
-    t_idx += 1
-    if s_idx + 1 >= len(s):
-        flag = True
-        continue
-    s_idx += 1
-
-# print(s)
-# print(t)
-'''
-AB
-BAABA
-'''
-if s == t:
-    print('1')
-else:
-    print('0')
+dfs(t)
+print(0)
