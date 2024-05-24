7014cb33dce7341bbbe65301c9dd579154e403e8
MKSonny <minson9@naver.com>
2024-05-24 16:34:19 +0900

    백준) 삼성 기출> 로봇 청소기: 벽 문제?

--- 백준/삼성 기출/로봇 청소기 5_24.py
+++ 백준/삼성 기출/로봇 청소기 5_24.py
@@ -7,13 +7,10 @@ l = []
 for _ in range(n):
     l.append(list(map(int, sys.stdin.readline().split())))
 
-for i in l:
-    print(i)
-
 # 반시계 방향
 # up, left, down, right
 dy = [-1, 0, 1, 0]
-dx = [0, -1, 0, 1]
+dx = [0, 1, 0, -1]
 
 '''
 5 5
@@ -25,56 +22,57 @@ dx = [0, -1, 0, 1]
 1 1 1 1 1
 '''
 
-def slice_end(d):
-    return (dy[-d:] + dy[0:4 - d], dx[-d:] + dx[0:4 - d])
-
-by_direction_dy = [[-1, 0, 1, 0]]
-by_direction_dx = [[0, -1, 0, 1]]
-
-for i in range(1, 4):
-    a, b = slice_end(i)
-    by_direction_dy.append(a)
-    by_direction_dx.append(b)
-
-# print(by_direction_dy)
-# print(by_direction_dx)
-'''
-만약 d가 0이라면 북쪽을 바라보고 있으므로 
-순서가 기존이랑 같음
-d가 1인 경우 동쪽을 바라보고 있으므로
-동 -> 북 -> 서 -> 남 순서로 주변 확인
-'''
 
 cnt = 0
-print()
+
+for_d = [0, 1, 2, 3]
 
 def dfs(y, x, d):
-    l[y][x] = 1
-
-    # for i in l:
-    #     print(i)
-    # print()
-
     global cnt
-    cnt += 1
+
+    if l[y][x] == 0:
+        cnt += 1
+        l[y][x] = cnt
+
+    for i in l:
+        print(i)
+    print()
+
     for i in range(4):
-        ny = y + by_direction_dy[d][i]
-        nx = x + by_direction_dx[d][i]
+        ny = y + dy[for_d[d - i]]
+        nx = x + dx[for_d[d - i]]
         if 0 <= ny < n and 0 <= nx < m:
             if l[ny][nx] == 0:
-                if d - i == -1:
-                    d = 3
-                elif d - i == -2:
-                    d = 2
-                dfs(ny, nx, d)
-
-dfs(start_y - 1, start_x - 1, 1)
-
-print()
-print(cnt)
-print()
-
-for i in l:
-    print(i)
+                # print(for_d[d - i])
+                dfs(ny, nx, for_d[d - i])
 
 
+    if d == 0:
+        y += 1
+    elif d == 1:
+        x -= 1
+    elif d == 2:
+        y -= 1
+    elif d == 3:
+        x += 1
+
+    if x < 0 or y < 0 or y >= m or x >= n:
+        print(cnt)
+        for i in l:
+            print(i)
+        exit()
+    else:
+        dfs(y, x, d)
+
+dfs(start_y, start_x, direction)
+'''
+7 7
+4 2 1
+1 1 1 1 1 1 1
+1 0 0 0 1 0 1
+1 0 1 1 0 0 1
+1 0 0 0 0 1 1
+1 0 0 1 0 0 1
+1 0 0 0 0 0 1
+1 1 1 1 1 1 1
+'''
