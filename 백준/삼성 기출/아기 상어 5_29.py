c953c01953d41199796b984400642235d842c83e
MKSonny <minson9@naver.com>
2024-05-29 18:33:32 +0900

    백준) 삼성 기출> 아기 상어:

---
+++ 백준/삼성 기출/아기 상어 5_29.py
@@ -0,0 +1,77 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+l = []
+
+for _ in range(n):
+    l.append(list(map(int, sys.stdin.readline().split())))
+
+# for i in l:
+#     print(i)
+
+dy = [-1, 0, 1, 0]
+dx = [0, -1, 0, 1]
+
+visited = [[0 for _ in range(n)] for _ in range(n)]
+ate_spot = [[False for _ in range(n)] for _ in range(n)]
+baby_shark = 2
+ate = 0
+total_ate = 0
+def bfs(y, x, visited):
+    global ate
+    global baby_shark
+    global ate_spot
+    global total_ate
+    q = deque([(y, x)])
+    # cnt = 1
+    while q:
+        y, x = q.popleft()
+        if total_ate == 4:
+            print('test')
+            for i in visited:
+                print(i)
+            exit()
+        if 0 < l[y][x] < baby_shark and not ate_spot[y][x]:
+            ate += 1
+            # for i in visited:
+            #     print(i)
+            # print()
+            if ate == baby_shark:
+                baby_shark += 1
+                ate = 0
+            t = visited[y][x]
+            # print(t)
+            # print(t, l[y][x], y, x)
+            # for i in visited:
+            #     print(i)
+            # print()
+            # if ate == 3:
+            #     break
+            visited = [[0 for _ in range(n)] for _ in range(n)]
+            ate_spot[y][x] = True
+            # print('ate', y, x)
+            visited[y][x] = t
+            # q = deque([(y, x)])
+            # for i in visited:
+            #     print(i)
+            # print()
+            bfs(y, x, visited)
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < n and 0 <= nx < n:
+                if visited[ny][nx] == 0:
+                    # cnt += 1
+                    visited[ny][nx] = visited[y][x] + 1
+                    q.append((ny, nx))
+    # print(cnt)
+
+for i in range(n):
+    for j in range(n):
+        if l[i][j] == 9:
+            bfs(i, j, visited)
+            break
+
+for i in visited:
+    print(i)
---
+++ 알고리즘 수업/8장 탐욕적 기법/복습/di 5_29.py
@@ -0,0 +1,19 @@
+INF = 9999
+vertex = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+weight = [
+    [0, 7, INF, INF, 3, 10, INF],
+    [7, 0, 4, 10, 2, 6, INF],
+    [INF, 4, 0, 2, INF, INF, INF],
+    [INF, 10, 2, 0, 11, 9, 4],
+    [3, 2, INF, 11, 0, 13, 5],
+    [10, 6, INF, 9, 13, 0, INF],
+    [INF, INF, INF, 4, 5, INF, 0]
+]
+
+visited = [False] * len(vertex)
+
+def get_min_vertex(l):
+    min_v = -1
+    for i in range(len(l)):
+        if not visited[i]:
+            min_v = min(min_v, )
