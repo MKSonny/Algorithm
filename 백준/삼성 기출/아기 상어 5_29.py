44ed337950dcabf8734fb21a343b8027fa432fd7
MKSonny <minson9@naver.com>
2024-05-30 17:35:25 +0900

    백준) 삼성 기출> 아기 상어: 검색해서 구현 성공

--- 백준/삼성 기출/아기 상어 5_29.py
+++ 백준/삼성 기출/아기 상어 5_29.py
@@ -7,74 +7,60 @@ l = []
 for _ in range(n):
     l.append(list(map(int, sys.stdin.readline().split())))
 
-# for i in l:
-#     print(i)
-
 dy = [0, -1, 0, 1]
 dx = [-1, 0, 1, 0]
 
-visited = [[0 for _ in range(n)] for _ in range(n)]
-ate_spot = [[False for _ in range(n)] for _ in range(n)]
-baby_shark = 2
-ate = 0
-total_ate = 0
-t = -1
-def bfs(y, x, visited):
-    global ate
-    global baby_shark
-    global ate_spot
-    global total_ate
-    global t
+shark_size = 2
+
+def bfs(y, x):
     q = deque([(y, x)])
-    # cnt = 1
+    visited = [[0 for _ in range(n)] for _ in range(n)]
+    t_list = []
+    visited[y][x] = 1
+    dist = 0
+
     while q:
         y, x = q.popleft()
-        # if total_ate == 4:
-        #     print('test')
-        #     for i in visited:
-        #         print(i)
-        #     exit()
-        if 0 < l[y][x] < baby_shark and not ate_spot[y][x]:
-            ate += 1
-            total_ate += 1
-            # print(y, x)
-            if ate == baby_shark:
-                baby_shark += 1
-                # print(y, x, baby_shark)
-                ate = 0
-            t = visited[y][x]
-            # print(t)
-            # print(t, l[y][x], y, x)
-            for i in visited:
-                print(i)
-            print()
-            # if ate == 3:
-            #     break
-            visited = [[0 for _ in range(n)] for _ in range(n)]
-            ate_spot[y][x] = True
-            # print('ate', y, x)
-            visited[y][x] = t
-            # q = deque([(y, x)])
-            # for i in visited:
-            #     print(i)
-            # print()
-            bfs(y, x, visited)
+
+        if dist == visited[y][x]:
+            return t_list, dist - 1
+
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
             if 0 <= ny < n and 0 <= nx < n:
-                if visited[ny][nx] == 0 and l[ny][nx] <= baby_shark:
-                    # cnt += 1
+                if visited[ny][nx] == 0 and l[ny][nx] <= shark_size:
                     visited[ny][nx] = visited[y][x] + 1
                     q.append((ny, nx))
-    # print('work')
+                    # 나모다 작은 물고기인 경우 먹음
+                    if 0 < l[ny][nx] < shark_size:
+                        t_list.append((ny, nx))
+                        dist = visited[ny][nx]
+
+    return t_list, dist - 1
+
 
 for i in range(n):
     for j in range(n):
         if l[i][j] == 9:
-            bfs(i, j, visited)
+            loc_y, loc_x = i, j
+            l[i][j] = 0
             break
-print(t)
-# print(total_ate)
-# for i in visited:
-#     print(i)
+
+cnt = ans = 0
+
+while True:
+    t_list, dist = bfs(loc_y, loc_x)
+    # print(t_list, dist)
+    if len(t_list) == 0:
+        break
+    t_list.sort(key=lambda x: (x[0], x[1]))
+    loc_y, loc_x = t_list[0]
+    l[loc_y][loc_x] = 0
+    cnt += 1
+    ans += dist
+    if shark_size == cnt:
+        shark_size += 1
+        cnt = 0
+
+print(ans)
