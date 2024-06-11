74e875b1a7f7ee5ae59ec9303b90614059baac2f
MKSonny <minson9@naver.com>
2024-06-11 18:39:34 +0900

    프로그래머스) 연습문제 Lv2> 두 큐 합 같게 만들기 코드 수정

--- 프로그래머스/연습문제 Lv2/두 큐 합 같게 만들기 6_11.py
+++ 프로그래머스/연습문제 Lv2/두 큐 합 같게 만들기 6_11.py
@@ -12,7 +12,12 @@ def solution(queue1, queue2):
 
     cnt = 0
     n = len(queue1)
-    maxx = (n + n) * 2
+    maxx = 3 * n
+
+    '''
+    제한은 다음과 같이 queue1의 원소가 queue2로 전부 이동하고, queue2의 원소만 queue1으로 이동하고, 
+    남은 queue2의 원소가 queue1으로 이동하는 경우로 len(q1) * 3 이 탐색 제한 범위가 될 수 있다.
+    '''
 
     if a == b:
         return 0
