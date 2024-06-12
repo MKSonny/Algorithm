d5dd29c64dcfb57f931c2751627698da1e0f97ed
MKSonny <minson9@naver.com>
2024-06-12 18:41:24 +0900

    프로그래머스) 연습문제 Lv2> 마법의 엘리베이터: 검색해서 해결

--- 프로그래머스/연습문제 Lv2/마법의 엘리베이터 6_12.py
+++ 프로그래머스/연습문제 Lv2/마법의 엘리베이터 6_12.py
@@ -3,12 +3,17 @@ def solution(storey):
     while storey != 0:
         # 2550
         t = 10 - (storey % 10)
-        if storey % 10 >= 5:
+        if storey % 10 > 5:
             storey += t
             answer += t
-        else:
+        elif storey % 10 < 5:
             answer += (storey % 10)
             storey -= (storey % 10)
+        else:
+            if (storey // 10) % 10 >= 5:
+                storey += 10
+            answer += t
+
         storey //= 10
 
     return answer
