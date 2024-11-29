625006d4e31e14eb89b150359370779884b9b4f4
MKSonny <minson9@naver.com>
2024-11-29 14:58:34 +0900

    백준) 삼성 기출> 마법사 상어와 비바라기 정답 보고 해결

--- 백준/삼성 기출/마법사 상어와 비바라기 11_29.py
+++ 백준/삼성 기출/마법사 상어와 비바라기 11_29.py
@@ -8,104 +8,53 @@ board = []
 for _ in range(n):
     board.append(list(map(int, sys.stdin.readline().split())))
 
-how = []
+moves = []
 
 for _ in range(m):
-    how.append(list(map(int, sys.stdin.readline().split())))
+    moves.append(list(map(int, sys.stdin.readline().split())))
 
 dy = [0, -1, -1, -1, 0, 1, 1, 1]
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 
-# 1. 시작점을 기준으로만 움직여도 됨 -> 모든 죄표를 리스트에 저장
-# 1). num은 dy[i] + num 이렇게
-# 2. 1씩 증가
-# 3. 대각선 검사
-# 4. 구름있던 칸 제외, 2이상인 곳 검사
-# 5. 2씩 감소
-# 6.
+# 첫 구름 좌표
+clouds = [(n - 1, 0), (n - 1, 1), (n - 2, 0), (n - 2, 1)]
 
-l = [[(n - 1, 0), (n - 1, 1), (n - 2, 0), (n - 2, 1)]]
+cross_y = [-1, 1, -1, 1]
+cross_x = [-1, 1, 1, -1]
 
-# for i in board:
-#     print(i)
-# print()
-
-c_dy = [-1, 1, -1, 1]
-c_dx = [-1, 1, 1, -1]
-
-# 대각선 확인
-def check(y, x):
-    cnt = 0
-    for i in range(4):
-        ny = y + c_dy[i]
-        nx = x + c_dx[i]
-        if 0 <= ny < n and 0 <= nx < n and board[ny][nx] > 0:
-            cnt += 1
-    return cnt
-
-# 이동
-
-
-# for i in board:
-#     print(i)
-# print()
-
-def bfs(y, x, visited):
-    q = deque([(y, x)])
-    dy = [-1, 1, 0, 0]
-    dx = [0, 0, -1, 1]
-    temp = []
-    while q:
-        y, x = q.popleft()
-        temp.append((y, x))
-        board[y][x] -= 2
-        visited[y][x] = True
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0 <= ny < n and 0 <= nx < n:
-                if not visited[ny][nx] and board[ny][nx] >= 2:
-                    q.append((ny, nx))
-    return temp
-
-
-def rain(l, d, num):
-    temp = []
-
-    for y, x in l:
-        ny = (y + (dy[d - 1] * num)) % n
-        nx = (x + (dx[d - 1] * num)) % n
-        # print(ny, nx)
-        board[ny][nx] += 1
-        # 좌표 수정
-
-
-        temp.append((ny, nx))
-
-        for y, x in temp:
-            board[y][x] += check(y, x)
-            visited[y][x] = True
-
-    for i in board:
-        print(i)
-    print()
-
-
-t = []
-
-for d, num in how:
+for d, s in moves:
+    moved_clouds = []
     visited = [[False for _ in range(n)] for _ in range(n)]
+    for y, x in clouds:
+        ny = (y + dy[d - 1] * s) % n
+        nx = (x + dx[d - 1] * s) % n
+        board[ny][nx] += 1 # 비 내리기
+        visited[ny][nx] = True
+        # 이동한 구름 죄표에 추가
+        moved_clouds.append((ny, nx))
 
-    for i in l:
-        rain(i, d, num)
+    for y, x in moved_clouds:
+        cnt = 0
+        # 이동한 후 대각 4 방향을 조사하여 count만큼 물의 양 추가
+        for d in range(4):
+            ny = y + cross_y[d]
+            nx = x + cross_x[d]
+            if 0 <= ny < n and 0 <= nx < n and board[ny][nx] > 0:
+                cnt += 1
+        board[y][x] += cnt
 
+    new_clouds = []
+    for y in range(n):
+        for x in range(n):
+            # 만약 이전 구름 좌표들과 겹치지 않고 2 보다 크다면
+            if not visited[y][x] and board[y][x] >= 2:
+                board[y][x] -= 2
+                new_clouds.append((y, x))
+    # clouds를 new_clouds로 교체
+    clouds = new_clouds
 
-
-    l = []
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] >= 2 and not visited[i][j]:
-                l.append(bfs(i, j, visited))
-
-for i in board:
-    print(i)
+answer = 0
+for i in range(n):
+    for j in range(n):
+        answer += board[i][j]
+print(answer)
