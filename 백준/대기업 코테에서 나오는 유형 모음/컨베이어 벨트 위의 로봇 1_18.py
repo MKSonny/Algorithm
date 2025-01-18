46928b5f78efcf4f7ab4d06ca75d5d19fca5f8f9
MKSonny <minson9@naver.com>
2025-01-18 11:44:45 +0900

    백준) 삼성 기출> 컨베이어 벨트 위의 로봇 이해

--- 백준/대기업 코테에서 나오는 유형 모음/컨베이어 벨트 위의 로봇 1_18.py
+++ 백준/대기업 코테에서 나오는 유형 모음/컨베이어 벨트 위의 로봇 1_18.py
@@ -1,7 +1,5 @@
 import sys
 from collections import deque
-from operator import truediv
-from tabnanny import check
 
 n, k = map(int, sys.stdin.readline().split())
 l = list(sys.stdin.readline().split())
@@ -9,26 +7,26 @@ l = list(map(int, l))
 l = deque(l)
 
 answer = 0
-belt = deque([False] * n)
+robot = deque([False] * n)
 
 while True:
-    answer += 1
-
     l.rotate(1)
-    belt.rotate(1)
+    robot.rotate(1)
 
-    belt[n - 1] = False
+    robot[n - 1] = False
 
     for i in range(n - 2, -1, -1):
-        if belt[i] and not belt[i + 1] and l[i + 1] > 0:
-            belt[i], belt[i + 1] = False, True
+        if l[i + 1] > 0 and robot[i + 1] == False and robot[i]:
+            robot[i], robot[i + 1] = False, True
             l[i + 1] -= 1
 
-    belt[n - 1] = False
+    robot[n - 1] = False
 
     if l[0] > 0:
-        belt[0] = True
+        robot[0] = True
         l[0] -= 1
+
+    answer += 1
 
     if l.count(0) >= k:
         break
