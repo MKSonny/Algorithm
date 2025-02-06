667d214bdca772923dade51f0ec40dae1239a3fb
MKSonny <minson9@naver.com>
2025-02-06 17:52:44 +0900

    프로그래머스) 연습문제 Lv2> 광물 캐기 정답 보고 해결

--- 백준/DP/연속합 2_6.py
+++ 백준/DP/연속합 2_6.py
@@ -4,39 +4,13 @@ n = int(sys.stdin.readline())
 
 lo = list(map(int, sys.stdin.readline().split()))
 
-maxx = -1
-total = 0
-al = sum(lo)
-# print(al)
+dp = [min(lo)] * n
+print(dp)
 
-for j in range(n - 1, 0, -1):
-    al -= lo[j]
-    total = al
-    # print('st', total)
-    for i in range(n - j):
-        total -= lo[i]
-        total += lo[i + j]
-        maxx = max(maxx, total)
-        # print(total)
-    # print()
+for i in range(n):
+    to = 0
+    for j in range(i, -1, -1):
+        to += lo[j]
+        dp[i] = max(dp[i], to)
 
-# for j in range(1, n):
-#     # total = sum(lo[: + j])
-#     # maxx = max(maxx, total)
-#     for i in range(n - j):
-#         total -= lo[i]
-#         total += lo[i + j]
-#         maxx = max(maxx, total)
-#         print(total)
-#     print()
-
-print(maxx)
-# l[0] + l[1] + l[2]
-# total = l[0] + l[1] + l[2]
-
-# total -= l[0]
-# total += l[3]
-# l[1] + l[2] + l[3]
-
-# l[0] + l[1] + l[2] + l[3]
-# l[1] + l[2] + l[3] + l[4]
+print(dp)
