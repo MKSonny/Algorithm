e32e8b8b17c499b8e77a9720b2094fa1a2f55ae7
MKSonny <minson9@naver.com>
2025-02-12 12:14:57 +0900

    백준) IT 기업 코테> 겹치는 건 싫어 틀림

---
+++ 백준/대기업 코테에서 나오는 유형 모음/겹치는 건 싫어 2_12.py
@@ -0,0 +1,38 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+lo = list(map(int, sys.stdin.readline().split()))
+
+'''
+12 2
+0 1 2 3 4 5 6
+3 2 5 5 5 4 4 1 7 1 2 5
+'''
+idx = 0
+cnt = 0
+di = {}
+di2 = {}
+maxx = 1
+
+while idx < n:
+    if lo[idx] not in di.keys():
+        di[lo[idx]] = 1
+        di2[lo[idx]] = idx
+    else:
+        di[lo[idx]] += 1
+
+    if di[lo[idx]] > k:
+        cnt = 1
+        # print('idx', idx)
+        idx = di2[lo[idx]]
+        di2[lo[idx]] = di2[lo[idx]] - 1
+
+        di = {lo[idx]: 1}
+    else:
+        cnt += 1
+
+    # print(di)
+    maxx = max(maxx, cnt)
+    idx += 1
+
+print(maxx)
