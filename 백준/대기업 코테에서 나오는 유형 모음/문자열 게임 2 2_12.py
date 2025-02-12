1f7e79f26dddd5a3d8eb4273751944c9771722e4
MKSonny <minson9@naver.com>
2025-02-12 15:57:04 +0900

    백준) IT 기업 코테> 문자열 게임 2 정답 보고 해결

--- 백준/대기업 코테에서 나오는 유형 모음/문자열 게임 2 2_12.py
+++ 백준/대기업 코테에서 나오는 유형 모음/문자열 게임 2 2_12.py
@@ -17,21 +17,13 @@ lt, rt = 0, 0
 def check(to, li, ori, m):
     # 1, 5, 6
     # to = []
-    print('li', li)
     for i in range(len(li)):
         te = li[i:i + m]
+        # print(te)
         cnt = 0
         # print('te1', te, end=' ')
         if len(te) < m:
             continue
-        print('te2', te)
-        # c = Counter(ori[te[0]:te[-1] + 1])
-        # te[0], te[-1] 사이에 'b'가 몇 개 있는지 검사 필요
-        for v in c.values():
-            if v >= m:
-                cnt += 1
-            if cnt >= 2:
-                continue
         to.append(te[-1] - te[0] + 1)
         # to.append(''.join(ori[te[0]:te[-1] + 1]))
     # print('a', to)
@@ -46,17 +38,17 @@ for i in range(len(lo)):
             di[c] = [idx]
         else:
             di[c].append(idx)
-    print(di)
+    # print(di)
     # print(ko[], i)
 
     for d in di:
         if len(di[d]) >= ko[i]:
+            # print(di)
             check(to, di[d], lo[i], ko[i])
 
     # print('to', to)
     if len(to) == 0:
         print(-1)
-        continue
-    print(min(to), max(to))
-
+    else:
+        print(min(to), max(to))
     di = {}
