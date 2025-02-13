4ced26516f33351896b6c7055eb984c00bfdc0c8
MKSonny <minson9@naver.com>
2025-02-13 13:33:00 +0900

    백준) IT 기업 코테> 숫자고르기 구현 실패

--- 백준/대기업 코테에서 나오는 유형 모음/숫자고르기 2_13.py
+++ 백준/대기업 코테에서 나오는 유형 모음/숫자고르기 2_13.py
@@ -1,67 +1,24 @@
 import sys
-from itertools import combinations
 
 n = int(sys.stdin.readline())
-li = []
+data = [0] + [int(sys.stdin.readline()) for _ in range(n)]
 
-for _ in range(n):
-    li.append(int(sys.stdin.readline()))
+# print(data)
+result = []
 
-to = []
-
-for idx, i in enumerate(li):
-    to.append((idx + 1, i))
-
-
-
-
-# ((1, 3), (3, 1), (5, 5))
-def check(te):
-    di = {}
-
-    for a, b in te:
-        if a not in di.keys():
-            di[a] = 1
-        else:
-            di[a] += 1
-
-        if b not in di.keys():
-            di[b] = 1
-        else:
-            di[b] += 1
-
-        if di[a] > 2 or di[b] > 2:
-            return False
-
-    for d in di:
-        if di[d] < 2:
-            return False
-
-    return True
+def dfs(v, i):
+    visited[v] = True
+    w = data[v]
+    if not visited[w]:
+        dfs(w, i)
+    elif visited[w] and w == i:
+        result.append(w)
 
 
-flag = False
+for i in range(1, n + 1):
+    visited = [False] * (n + 1)
+    dfs(i, i)
 
-
-for m in range(n - 1, 0, -1):
-    c = combinations(to, m)
-
-    for i in c:
-        if check(i):
-            # print(i)
-            flag = True
-
-            an = []
-            for k in i:
-                an.append(k[0])
-
-            an.sort()
-
-            print(m)
-            for a in an:
-                print(a)
-
-            break
-
-    if flag: break
-    # print(i)
+print(len(result))
+result.sort()
+for i in result: print(i)
