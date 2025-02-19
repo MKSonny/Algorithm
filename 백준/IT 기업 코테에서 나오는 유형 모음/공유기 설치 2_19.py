646d5bd0ed9e408f4aaa88f7e3eb0878a6603353
MKSonny <minson9@naver.com>
2025-02-19 18:12:20 +0900

    백준) IT 기업 코테> 공유기 설치 구현 실패

---
+++ 백준/IT 기업 코테에서 나오는 유형 모음/공유기 설치 2_19.py
@@ -0,0 +1,25 @@
+import sys
+
+n, c = map(int, sys.stdin.readline().split())
+li = []
+
+for _ in range(n):
+    li.append(int(sys.stdin.readline()))
+
+li.sort()
+
+lt = 0
+rt = n - 1
+
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if li[mid] in li
+
+# index값은 관계없음
+
+
+# 1 3 4 1000 100000000
+# 1 ~ 100000000
+
+
+print(li)
