35339c06ecc3041f31042a46e27cb175bba09189
MKSonny <minson9@naver.com>
2025-02-19 17:50:00 +0900

    백준) IT 기업 코테> 전구와 스위치 구현 실패

--- 백준/IT 기업 코테에서 나오는 유형 모음/전구와 스위치 2_19.py
+++ 백준/IT 기업 코테에서 나오는 유형 모음/전구와 스위치 2_19.py
@@ -2,40 +2,16 @@ import sys
 # sys.setrecursionlimit(100000)
 
 n = int(sys.stdin.readline())
-li = list(map(int, sys.stdin.readline().strip()))
-want = list(map(int, sys.stdin.readline().strip()))
+bulb = list(map(int, sys.stdin.readline().strip()))
+target = list(map(int, sys.stdin.readline().strip()))
 
-dx = [-1, 0, 1]
-min_cnt = float('inf')
-flag = False
-
-# dfs에 visited를 줄 수 없어 어려움
-
-def dfs(temp, level, cnt):
-    cnt += 1
-    if temp == want:
-        # print('temp', temp, cnt)
-        return True
-
-    if level == n or level < 0:
-        return
-
-
-    for i in range(3):
-        nx = level + dx[i]
-        if 0 <= nx < n:
-            if temp[nx] == 1:
-                temp[nx] = 0
-            else:
-                temp[nx] = 1
-
-    print(level, temp, cnt)
-    dfs(temp, level - 1, cnt)
-    # dfs(temp, level, cnt)
-    dfs(temp, level + 1, cnt)
-
-dfs(li, 0, 0)
-# if flag == False:
-#     print(-1)
-# else:
-#     print(min_cnt)
+def change(A, B):
+    A_copy = A[:]
+    press = 0
+    for i in range(1, n):
+        if A_copy[i - 1] == B[i - 1]:
+            continue
+        press += 1
+        for j in range(i - 1, i + 2):
+            if j < n:
+                A_copy[j] = 1 - A_copy[j]
