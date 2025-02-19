340657a719170d71e8ed70158655ab66f9f3849b
MKSonny <minson9@naver.com>
2025-02-19 16:29:34 +0900

    백준) IT 기업 코테> 숫자고르기 정답 보고 해결

--- 백준/IT 기업 코테에서 나오는 유형 모음/숫자고르기 2_13.py
+++ 백준/IT 기업 코테에서 나오는 유형 모음/숫자고르기 2_13.py
@@ -2,12 +2,11 @@ import sys
 
 n = int(sys.stdin.readline())
 data = [0] + [int(sys.stdin.readline()) for _ in range(n)]
-
 # print(data)
 result = []
 
 def dfs(v, i):
-    visited[v] = True
+    visited[v] = True # 여기 if not visited[v] 조건을 넣지 않은 이유?
     w = data[v]
     if not visited[w]:
         dfs(w, i)
@@ -19,6 +18,6 @@ for i in range(1, n + 1):
     visited = [False] * (n + 1)
     dfs(i, i)
 
-print(len(result))
 result.sort()
+print(len(result))
 for i in result: print(i)
