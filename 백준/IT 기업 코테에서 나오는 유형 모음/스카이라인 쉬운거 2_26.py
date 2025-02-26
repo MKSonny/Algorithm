bc7c5fc719399b977baee9087b37830d7cd078d5
MKSonny <minson9@naver.com>
2025-02-26 15:07:23 +0900

    백준) IT 기업 코테> 스카이라인 쉬운거 정답 보고 해결

--- 백준/IT 기업 코테에서 나오는 유형 모음/스카이라인 쉬운거 2_26.py
+++ 백준/IT 기업 코테에서 나오는 유형 모음/스카이라인 쉬운거 2_26.py
@@ -1,30 +1,23 @@
 import sys
-from collections import deque
 
 n = int(sys.stdin.readline())
-loc = deque([0])
-max_height = -1
-max_width = -1
+answer = 0
 
-for _ in range(n):
-    a, b = map(int, sys.stdin.readline().split())
-    loc.append(b)
+loc = []
+
+for i in range(n):
+    loc.append(int(sys.stdin.readline().split()[1]))
 loc.append(0)
 
-# loc = deque([0, 2, 3, 1, 0])
-first = loc.popleft()
+s = [0]
 
-answer = 0
-cnt = 0
-
-while loc:
-    h = loc.popleft()
-    if h < first:
-        answer += cnt
-        cnt = 0
-    else:
-        # print(h)
-        cnt += 1
-    first = h
+for p in loc:
+    height = p
+    while s[-1] > p:
+        if s[-1] != height:
+            answer += 1
+            height = s[-1]
+        s.pop()
+    s.append(p)
 
 print(answer)
