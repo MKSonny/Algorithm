d5abcc00de4023e8127fd56601f22ed2d1ed621d
MKSonny <minson9@naver.com>
2025-02-27 13:32:45 +0900

    백준) IT 기업 코테> 좋다 정답 보고 해결

--- 백준/IT 기업 코테에서 나오는 유형 모음/좋다 2_27.py
+++ 백준/IT 기업 코테에서 나오는 유형 모음/좋다 2_27.py
@@ -28,34 +28,3 @@ for i in range(n):
     l.insert(i, t)
 
 print(answer)
-'''
-5
--1 0 1 2 3
-
-lt = 0
-rt = 3
-mid = 1
-
--1 + 2 = 1 < 3
-lt = mid + 1
-lt = 1
-rt = 3
-
-0[1] + 2[3] = 2 < 3
-lt = mid + 1 -> 3 
-rt = 3
-
-=========
-lt = 0
-rt = 3
-mid = 1
-
-lt = mid
-lt = 1
-rt = 3
-
-lt = mid = 2
-rt = 3
-1[2] + 2[3] = 3 찾음
-자기 자신은 제외해야 한다
-'''
