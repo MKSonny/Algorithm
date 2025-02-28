5041d7b7c684b32478b6d38794fdffa68ca872f3
MKSonny <minson9@naver.com>
2025-02-28 09:29:59 +0900

    백준) 단계별로 풀어보기> 스택 2

---
+++ 백준/IT 기업 코테에서 나오는 유형 모음/스택 2 2_28.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(sys.stdin.readline())
+s = []
+for _ in range(n):
+    c = sys.stdin.readline().split()
+    if c[0] == '4':
+        if len(s) == 0:
+            print(1)
+        else:
+            print(0)
+    elif c[0] == '1':
+        s.append(c[1])
+    elif c[0] == '3':
+        print(len(s))
+    elif c[0] == '5':
+        if len(s) == 0:
+            print(-1)
+        else:
+            print(s[-1])
+    elif c[0] == '2':
+        if len(s) == 0:
+            print(-1)
+        else:
+            print(s.pop())
