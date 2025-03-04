046a211a169258d6087b2165aea8ef5c21a743d5
MKSonny <minson9@naver.com>
2025-03-04 18:03:33 +0900

    백준) IT 기업 코테> List of Unique Numbers 정답 보고 해결

--- 백준/IT 기업 코테에서 나오는 유형 모음/List of Unique Numbers 3_4.py
+++ 백준/IT 기업 코테에서 나오는 유형 모음/List of Unique Numbers 3_4.py
@@ -44,4 +44,18 @@ import sys
 
 n = int(sys.stdin.readline())
 l = list(map(int, sys.stdin.readline().split()))
-s = []
+
+answer = 0
+start, end = 0, 0
+seq = [False for _ in range(1000001)]
+
+while start < n and end < n:
+    if not seq[l[end]]:
+        seq[l[end]] = True
+        end += 1
+        answer += (end - start)
+    else:
+        seq[l[start]] = False
+        start += 1
+
+print(answer)
