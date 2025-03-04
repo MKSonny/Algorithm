bf0286232344df904c30276f6431f18e7c65ed3d
MKSonny <minson9@naver.com>
2025-03-04 17:00:23 +0900

    백준) IT 기업 코테> 문자열 폭발 정답 보고 해결

---
+++ 백준/IT 기업 코테에서 나오는 유형 모음/문자열 폭발 3_4.py
@@ -0,0 +1,17 @@
+import sys
+
+s = list(sys.stdin.readline().rstrip())
+target = list(sys.stdin.readline().rstrip())
+m = len(target)
+st = []
+
+for i in s:
+    st.append(i)
+    if st[len(st) - m:len(st)] == target:
+        for _ in range(m):
+            st.pop()
+
+if st:
+    print(*st, sep='')
+else:
+    print("FRULA")
