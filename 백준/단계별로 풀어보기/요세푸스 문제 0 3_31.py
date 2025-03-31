3a1e78cc760eb5983b1aabbf6f31858df25361a5
MKSonny <minson9@naver.com>
2025-03-31 11:02:01 +0900

    백준) 단계별로 풀어보기> 요세푸스 문제 0 더 간단한 구현

--- 백준/단계별로 풀어보기/요세푸스 문제 0 3_31.py
+++ 백준/단계별로 풀어보기/요세푸스 문제 0 3_31.py
@@ -5,38 +5,15 @@ n, k = map(int, sys.stdin.readline().split())
 q = [i for i in range(1, n + 1)]
 q = deque(q)
 
-# 1 2 3 4 5 6 7
-# 1 2 4 5 6 7
-# 1 2 4 5 7
-
 cnt = 0
-t = []
 answer = []
 
-while True:
-    # print(q, t)
-    if len(q) == 0:
-        break
-    # if len(q) == 1:
-    #     print(q.popleft())
-    #     break
-    a = q.popleft()
-    cnt += 1
-    if cnt == k:
-        answer.append(a)
-        cnt = 0
-        q.extend(t)
-        t.clear()
-        continue
-    if len(q) == 0:
-        t.append(a)
-        q.extend(t)
-        t.clear()
-        continue
-
-    t.append(a)
+while len(q):
+    for _ in range(k - 1):
+        q.append(q.popleft())
+    answer.append(str(q.popleft()))
 
 print('<', end='')
-for i in range(len(answer) - 1):
+for i in range(n - 1):
     print(answer[i], end=', ')
-print(str(answer[-1]) + '>')
+print(answer[-1] + '>')
