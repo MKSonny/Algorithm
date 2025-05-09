3922baf5954e5797b51cdbe27ac5067526d9712a
MKSonny <minson9@naver.com>
2025-05-09 13:28:42 +0900

    프로그래머스) 연습문제 Lv3> 스티커 모으기(2) 정답 보고 해결

---
+++ 프로그래머스/연습문제 Lv3/스티커 모으기(2) 5_9.py
@@ -0,0 +1,17 @@
+def solution(sticker):
+    answer = 0
+
+    if len(sticker) == 1:
+        return sticker.pop()
+
+    size = len(sticker)
+
+    dp1 = [0] + sticker[:-1]
+    for i in range(2, size):
+        dp1[i] = max(dp1[i - 2] + dp1[i], dp1[i - 1])
+
+    dp2 = [0] + sticker[1:]
+    for i in range(2, size):
+        dp2[i] = max(dp2[i - 2] + dp2[i], dp2[i - 1])
+
+    return max(dp1[-1], dp2[-1])
