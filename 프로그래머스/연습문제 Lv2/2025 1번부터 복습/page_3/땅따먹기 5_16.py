a7e3fb236d2771de7196ccd6dd5108e2bbdc2fb1
MKSonny <minson9@naver.com>
2025-05-16 14:15:42 +0900

    백준) 단계별로 풀어보기> 땅따먹기 구현 성공

---
+++ 프로그래머스/연습문제 Lv2/2025 1번부터 복습/page_3/땅따먹기 5_16.py
@@ -0,0 +1,21 @@
+maxx = -1
+
+
+def solution(land):
+    answer = 0
+
+    dp = [[0 for _ in range(4)] for _ in range(len(land))]
+
+    for i in range(4):
+        dp[0][i] = land[0][i]
+
+    for m in range(1, len(land)):
+        for i in range(4):
+            for j in range(4):
+                if j != i:
+                    dp[m][j] = max(dp[m][j], dp[m - 1][i] + land[m][j])
+
+    #     for i in dp:
+    #         print(i)
+
+    return max(dp[-1])
