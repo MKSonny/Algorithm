56661b3e23d01af0e6bf74deecbd0d6c737d9bd2
MKSonny <minson9@naver.com>
2025-05-31 21:36:52 +0900

    프로그래머스) 연습문제 Lv2) page 4> 숫자 카드 나누기 정답 보고 해결

--- 프로그래머스/연습문제 Lv2/2025 1번부터 복습/page_4/숫자 카드 나누기 5_31.py
+++ 프로그래머스/연습문제 Lv2/2025 1번부터 복습/page_4/숫자 카드 나누기 5_31.py
@@ -1,35 +1,25 @@
+import math
+
+
 def solution(arrayA, arrayB):
-    answer = [0]
+    answer = 0
+    gA = arrayA[0]
+    gB = arrayB[0]
+    ta = []
+    tb = []
 
-    l = arrayA + arrayB
+    for i in range(len(arrayA)):
+        gA = math.gcd(gA, arrayA[i])
+        gB = math.gcd(gB, arrayB[i])
 
-    lt = 0
-    rt = len(l) - 1
+    for i in arrayA:
+        if i % gB == 0:
+            gB = 0
+            break
 
-    for idx in range(2, min(arrayA) + 1):
-        lt = 0
-        rt = len(l) - 1
-        cnt = 0
+    for i in arrayB:
+        if i % gA == 0:
+            gA = 0
+            break
 
-        while l[lt] % idx == 0 and l[rt] % idx != 0:
-            lt += 1
-            rt -= 1
-            cnt += 1
-
-        if cnt == len(arrayA):
-            answer.append(idx)
-
-    for idx in range(2, min(arrayB) + 1):
-        lt = 0
-        rt = len(l) - 1
-        cnt = 0
-
-        while l[lt] % idx != 0 and l[rt] % idx == 0:
-            lt += 1
-            rt -= 1
-            cnt += 1
-
-        if cnt == len(arrayA):
-            answer.append(idx)
-
-    return max(answer)
+    return max(gB, gA)
