64074cd8bd18d74d42a131e3605c50e004d8b4b1
MKSonny <minson9@naver.com>
2025-06-07 22:56:21 +0900

    백준) 단계별로 풀어보기> 실버 5> 분수 찾기 정답 보고 해결

--- 백준/단계별로 풀어보기/2025/분수찾기 6_7.py
+++ 백준/단계별로 풀어보기/2025/분수찾기 6_7.py
@@ -14,38 +14,13 @@ n = int(sys.stdin.readline())
 1, 2, 6, 7, 15
 '''
 # [1, 2(2, 3), 3(4, 5, 6), 4(7, 8, 9, 10), 5]
-cnt = 1
-g = 1
-g_cnt = 0
+line = 1
 
-i, j = 1, 1
-toggle = False
+while n > line:
+    n -= line
+    line += 1
 
-while True:
-    if cnt == n:
-        # print(g, n, i, j)
-        print(str(i) + '/' + str(j))
-        break
-
-    g_cnt += 1
-
-    if toggle:
-        j -= 1
-        i += 1
-    else:
-        j += 1
-        i -= 1
-
-    if g_cnt == g:
-        toggle = not toggle
-        g += 1
-        g_cnt = 0
-
-        if toggle:
-            j = g
-            i = 1
-        else:
-            i = g
-            j = 1
-
-    cnt += 1
+if line % 2 == 0:
+    print(str(n) + '/' + str(line - n + 1))
+else:
+    print(str(line - n + 1) + '/' + str(n))
